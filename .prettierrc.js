0a09ee3640275de70c2acc8e2df54c881c2f8c01
Jesse Clark <hello@developerjesse.com>
2021-09-29 14:16:16 +0300

    Add initial file and update lint/prettier to preferences.

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,6 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
+  semi: false,
+  bracketSpacing: true,
 };
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+interface Interface {}
+
+const App: React.FC<Interface> = () => {
+  return <Text>Hello sWallet</Text>
+}
+
+export default App
