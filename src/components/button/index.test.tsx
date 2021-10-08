22361bdaca8164de2c57f6302c5e210893e8e1a7
Jesse Clark <hello@developerjesse.com>
2021-10-08 17:59:30 +0300

    Add simple test for the button to check functionality.

--- src/components/button/index.test.tsx
+++ src/components/button/index.test.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
-
+import { render, fireEvent } from '@testing-library/react-native'
 import Button from './index'
 
-it('renders correctly with defaults', () => {
-  const button = renderer
-    .create(<Button title="hello" onPress={() => console.log('click')} />)
-    .toJSON()
-  // const button = mount(<Button title="hello" onPress={() => console.log('click')} />)
-  expect(button).toBeDefined()
+describe('button', () => {
+  it('renders correctly with defaults', () => {
+    const onPress = jest.fn()
+    const { getByText } = render(<Button title="hello" onPress={onPress} />)
+
+    expect(getByText('hello')).toBeDefined()
+
+    fireEvent.press(getByText('hello'))
+    expect(onPress).toBeCalledTimes(1)
+  })
 })
