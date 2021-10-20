56df04bdcde96ffbc2b217e52f56f5caba5de5eb
Jesse Clark <hello@developerjesse.com>
2021-10-20 15:18:16 +0300

    Fix test, smartWalletAddress is no longer a promise.

--- src/screens/receive/ReceiveScreen.test.tsx
+++ src/screens/receive/ReceiveScreen.test.tsx
@@ -12,9 +12,7 @@ describe('ReceiveScreen', () => {
   const route = {
     params: {
       account: {
-        getSmartAddress: jest.fn(() =>
-          Promise.resolve('0xbd4c8e11cf2c560382e0dbd6aeef538debf1d449'),
-        ),
+        smartWalletAddress: '0xbd4c8e11cf2c560382e0dbd6aeef538debf1d449',
       },
     },
   }
@@ -25,10 +23,6 @@ describe('ReceiveScreen', () => {
 
   it('renders', async () => {
     const { getAllByText, rerender } = render(<ReceiveScreen route={route} />)
-
-    await waitFor(() =>
-      expect(route.params.account.getSmartAddress).toHaveBeenCalledTimes(1),
-    )
 
     act(() => {
       rerender(<ReceiveScreen route={route} />)
