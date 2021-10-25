22f832f57d8ae7331491a346e52c6f55caa1876a
Jesse Clark <hello@developerjesse.com>
2021-10-25 16:08:18 +0300

    Add signTypedData with temp request.

--- src/App.tsx
+++ src/App.tsx
@@ -34,15 +34,55 @@ const WalletApp: React.FC<Interface> = ({ navigation }) => {
     setMnemonic(context.getMnemonic())
   }, [context.wallets])
 
-  /*
-  const addAccount = () => {
-    if (wallet) {
-      wallet
-        ?.getAccount(accounts.length)
-        .then(account => setAccounts(accounts.concat(account)))
+  // TEMP - Add a request to the que WITHOUT going through the wallet!
+  const signedTypedData = () => {
+    const typedDataRequest = {
+      domain: {
+        chainId: 31,
+        name: 'rLogin sample app',
+        verifyingContract: '0x285b30492a3f444d7bf75261a35cb292fc8f41a6',
+        version: '1',
+      },
+      message: {
+        contents: 'Welcome to rLogin!',
+        num: 1500,
+        person: {
+          firstName: 'jesse',
+          lastName: 'clark',
+        },
+      },
+      // Refers to the keys of the *types* object below.
+      primaryType: 'Sample',
+      types: {
+        EIP712Domain: [
+          { name: 'name', type: 'string' },
+          { name: 'version', type: 'string' },
+          { name: 'chainId', type: 'uint256' },
+          { name: 'verifyingContract', type: 'address' },
+        ],
+        // Not an EIP712Domain definition
+        Sample: [
+          { name: 'contents', type: 'string' },
+          { name: 'num', type: 'uint256' },
+          { name: 'person', type: 'Person' },
+        ],
+        Person: [
+          { name: 'firstName', type: 'string' },
+          { name: 'lastName', type: 'string' },
+        ],
+      },
     }
+
+    const request = {
+      type: 'signTypedData',
+      payload: typedDataRequest,
+      confirm: () => console.log('CONFIRMED!'),
+      reject: () => console.log('REJECTED!'),
+    }
+
+    // @ts-ignore
+    context.addUxInteraction(request)
   }
-  */
 
   const seeSmartWallet = (account: RIFWallet) =>
     // @ts-ignore
@@ -93,6 +133,7 @@ const WalletApp: React.FC<Interface> = ({ navigation }) => {
                 }
                 title="Balances"
               />
+              <Button onPress={signedTypedData} title="Sign Typed Data" />
             </View>
           )
         })}
---
+++ src/modal/SignTypedDataModal.tsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import Button from '../components/button'
+import { Header2, Paragraph } from '../components/typography'
+import { Request } from '../lib/core'
+import { styles as sharedStyles } from './ModalComponent'
+
+// Temp: type to be refactored with the changes in the core
+export interface SignTypedDataRequest extends Request {
+  type: 'signTypedData'
+  payload: any
+}
+
+interface Interface {
+  request: SignTypedDataRequest
+  closeModal: () => void
+}
+
+const formatter = (data: any) =>
+  Object.keys(data).map((key: string) => (
+    <View key={key} style={styles.nested} testID="Formatter.Row">
+      <Text style={styles.heading} testID="Text.Heading">
+        {key}
+      </Text>
+      {typeof data[key] !== 'object' ? (
+        <Text style={styles.value} testID="Value">
+          {data[key]}
+        </Text>
+      ) : (
+        formatter(data[key])
+      )}
+    </View>
+  ))
+
+const SignTypedDataModal: React.FC<Interface> = ({ request, closeModal }) => {
+  const approve = () => {
+    request.confirm()
+    closeModal()
+  }
+
+  const reject = () => {
+    request.confirm()
+    closeModal()
+  }
+
+  return (
+    <View>
+      <Header2>Sign Typed Data</Header2>
+
+      <View style={sharedStyles.row}>
+        <Paragraph>Name: {request.payload.domain.name}</Paragraph>
+      </View>
+
+      <ScrollView style={styles.message}>
+        {formatter(request.payload.message)}
+      </ScrollView>
+
+      <View style={sharedStyles.row}>
+        <View style={sharedStyles.column}>
+          <Button
+            onPress={approve}
+            title="Sign Message"
+            testID="Button.Confirm"
+          />
+        </View>
+        <View style={sharedStyles.column}>
+          <Button onPress={reject} title="Reject" testID="Button.Reject" />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export const styles = StyleSheet.create({
+  message: {
+    backgroundColor: '#f1f1f1',
+    borderWidth: 1,
+    borderColor: '#d1d1d1',
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginTop: 10,
+    marginBottom: 20,
+    maxHeight: '75%',
+  },
+  nested: {
+    marginLeft: 20,
+    marginTop: 5,
+  },
+  heading: {
+    fontWeight: 'bold',
+    width: '100%',
+  },
+  value: {
+    paddingLeft: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+})
+
+export default SignTypedDataModal
