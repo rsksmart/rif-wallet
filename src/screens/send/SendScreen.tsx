e82c87b824f482e2bcf1d31f9892c7d5d134c1ac
Luis Chavarría <luis.chavarria@iovlabs.org>
2021-11-05 11:21:00 -0600

    fix: lint code

--- src/screens/send/SendScreen.tsx
+++ src/screens/send/SendScreen.tsx
@@ -24,9 +24,7 @@ export const SendScreen: React.FC<ScreenProps<'Send'> & ScreenWithWallet> = ({
   const smartAddress = wallet.smartWalletAddress
 
   const [to, setTo] = useState('0x1D4F6A5FE927f0E0e4497B91CebfBcF64dA1c934')
-  const [selectedSymbol] = useState(
-    route.params?.token || 'tRIF',
-  )
+  const [selectedSymbol] = useState(route.params?.token || 'tRIF')
   const [availableTokens, setAvailableTokens] = useState<IToken[]>()
   const [amount, setAmount] = useState('')
   const [tx, setTx] = useState<ContractReceipt | null>(null)
