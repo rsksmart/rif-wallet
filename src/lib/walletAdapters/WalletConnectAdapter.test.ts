8b588c8d3127e1e2a3354c193ec2d6863ee9a04b
Christian Escalante <chescalante@gmail.com>
2021-11-10 11:05:18 -0300

    added sign typed data and test fix

--- src/lib/walletAdapters/WalletConnectAdapter.test.ts
+++ src/lib/walletAdapters/WalletConnectAdapter.test.ts
@@ -44,12 +44,29 @@ describe('Wallet Connect Adapter', () => {
     expect(result).toContain('0x')
   })
 
-  test('personal sign', async () => {
+  // method not supported by ganache-cli
+  // eslint-disable-next-line jest/no-disabled-tests
+  test.skip('personal sign', async () => {
     const from = await signer?.getAddress()
     const message = '0x68656c6c6f20776f726c6421'
 
     const method = 'personal_sign'
     const params = [message, from]
+
+    const result = await adapter?.handleCall(method, params)
+
+    expect(result).toBeDefined()
+    expect(result).toContain('0x')
+  })
+
+  // method not supported by ganache-cli
+  // eslint-disable-next-line jest/no-disabled-tests
+  test.skip('sign typed data', async () => {
+    const method = 'eth_signTypedData'
+    const params = [
+      '0x5cD710d005a37785736eBA7d0395E73869edb8E2',
+      '{"domain":{"chainId":31,"name":"Ether Mail","version":"1"},"message":{"contents":"{     from: {         name: \'Cow\',         wallet: \'0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826\'     },     to: {         name: \'Bob\',         wallet: \'0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB\'     },     contents: \'Hello, Bob!\' }","from":"Diego","to":"Cesar"},"primaryType":"Mail","types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"}],"Mail":[{"name":"from","type":"string"},{"name":"to","type":"string"},{"name":"contents","type":"string"}]}}',
+    ]
 
     const result = await adapter?.handleCall(method, params)
 
