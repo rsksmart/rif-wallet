a1b14f496795425b209bc19fa673ac6fefbafe3c
Ilan <ilanolkies@outlook.com>
2021-11-18 23:47:49 -0300

    Refactor
    - Removes rskjs-util - Moves addr input to components - Disable button when addr invalid - Wrap addresses lib in a single file - Add types for @rsksmart/rsk-utils

---
+++ src/components/address/lib.test.ts
@@ -0,0 +1,23 @@
+import { AddressValidationMessage, validateAddress } from './lib'
+import { testnetCase } from './testCase'
+
+describe('validate address', () => {
+  describe('testnet', () => {
+    test('invalid address', () =>
+      expect(validateAddress(testnetCase.invalid, 31)).toEqual(
+        AddressValidationMessage.INVALID_ADDRESS,
+      ))
+    test('valid lowercase address', () =>
+      expect(validateAddress(testnetCase.checksummed, 31)).toEqual(
+        AddressValidationMessage.VALID,
+      ))
+    test('valid checksummed address', () =>
+      expect(validateAddress(testnetCase.lower, 31)).toEqual(
+        AddressValidationMessage.VALID,
+      ))
+    test('invalid checksummed address', () =>
+      expect(validateAddress(testnetCase.wrongChecksum, 31)).toEqual(
+        AddressValidationMessage.INVALID_CHECKSUM,
+      ))
+  })
+})
---
+++ src/components/address/lib.ts
@@ -0,0 +1,37 @@
+import {
+  isAddress,
+  isValidChecksumAddress,
+  toChecksumAddress,
+} from '@rsksmart/rsk-utils'
+
+export enum AddressValidationMessage {
+  INVALID_ADDRESS = 'Invalid address',
+  INVALID_CHECKSUM = 'Invalid checksum',
+  VALID = '',
+}
+
+/**
+ * validate addresses according to rskip-60
+ * https://github.com/rsksmart/RSKIPs/blob/master/IPs/RSKIP60.md
+ * @param {string} address to validate
+ * @param {number} chainId defined in erip-155
+ * @returns {string} null if it's valid and an error message if it is not
+ */
+export const validateAddress = (address: string, chainId = 31) => {
+  if (!address) {
+    return AddressValidationMessage.VALID
+  }
+
+  if (!isAddress(address)) {
+    return AddressValidationMessage.INVALID_ADDRESS
+  }
+  if (
+    !isValidChecksumAddress(address, chainId) &&
+    address !== address.toLowerCase()
+  ) {
+    return AddressValidationMessage.INVALID_CHECKSUM
+  }
+  return AddressValidationMessage.VALID
+}
+
+export { toChecksumAddress }
