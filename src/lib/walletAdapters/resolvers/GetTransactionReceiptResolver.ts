0609a988bee8fd6891c365b4226c3abb4eecb79c
Christian Escalante <chescalante@gmail.com>
2021-12-01 16:43:16 -0300

    fix bug tx receipt resolver

--- src/lib/walletAdapters/resolvers/GetTransactionReceiptResolver.ts
+++ src/lib/walletAdapters/resolvers/GetTransactionReceiptResolver.ts
@@ -1,6 +1,5 @@
 import { Signer } from 'ethers'
 import { IResolver } from '../RPCAdapter'
-
 export class GetTransactionReceiptResolver implements IResolver {
   private signer: Signer
   public methodName = 'eth_getTransactionReceipt'
@@ -12,6 +11,13 @@ export class GetTransactionReceiptResolver implements IResolver {
   async resolve(params: any[]) {
     const txHash = params[0]
 
-    return this.signer.provider?.getTransactionReceipt(txHash)
+    let result = await this.signer.provider?.getTransactionReceipt(txHash)
+
+    if (!result) {
+      const tx = await this.signer.provider?.getTransaction(txHash)
+      result = await tx?.wait()
+    }
+
+    return result
   }
 }
