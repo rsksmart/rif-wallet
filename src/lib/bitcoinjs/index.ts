bf3e38eebb3613952cd5b93c6727442e27d65a49
Luis Chavarría <luis.chavarria@iovlabs.org>
2021-12-02 00:09:51 -0600

    fix: remove log

--- src/lib/bitcoinjs/index.ts
+++ src/lib/bitcoinjs/index.ts
@@ -2,7 +2,6 @@ import * as englishWordlistObj from '../../lib/bitcoinjs/bip39/wordlists/english
 const englishWordlist = Object.values(englishWordlistObj)
 
 export const isWordlistValid = (wordlistToValidate: string[]) => {
-  console.log({ englishWordlist })
   for (let i = 0; wordlistToValidate.length > i; i++) {
     if (!englishWordlist.includes(wordlistToValidate[i])) {
       return false
