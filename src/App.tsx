51de129562e1baaef2fd1de27aae4637c87ed960
Jesse Clark <hello@developerjesse.com>
2021-12-06 14:48:09 +0200

    Reorder code and handle possibilities.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { KeyManagementSystem, OnRequest, RIFWallet } from './lib/core'
 import { getKeys, hasKeys, saveKeys, deleteKeys } from './storage/KeyStore'
 import { jsonRpcProvider } from './lib/jsonRpcProvider'
 import { i18nInit } from './lib/i18n'
-import { Paragraph } from './components'
+import { Loading } from './components'
 import { AppContext } from './Context'
 import { RifWalletServicesFetcher } from './lib/rifWalletServices/RifWalletServicesFetcher'
 import { AbiEnhancer } from './lib/abiEnhancer/AbiEnhancer'
@@ -56,65 +56,68 @@ const App = () => {
     setKMS(newKms)
   }
 
-  AppState.addEventListener('change', (newState: string) => {
+  // TEMP TEMP
+  const logUpdateState = (newState: any) => {
+    console.log('state change:', newState)
+    // @ts-ignore
+    setAppState(newState)
+  }
+
+  const handleAppStateChange = (newState: string) => {
     if (newState !== 'active') {
-      return setAppState('BACKGROUND')
+      resetAppState()
+      return logUpdateState('BACKGROUND')
     }
 
-    setAppState('LOADING')
+    appIsActive()
+  }
 
-    // active, but for how log ;-)
-    hasPin().then((answer: boolean | null) =>
-      setAppState(answer ? 'LOCKED' : 'READY'),
-    )
-  })
+  /**
+   * App is active and ready to check if it has keys and has pin
+   */
+  const appIsActive = () => {
+    logUpdateState('LOADING')
 
-  const init = async () => {
-    if (await hasKeys()) {
-      const serializedKeys = await getKeys()
-      // eslint-disable-next-line no-shadow
-      const { kms, wallets } = KeyManagementSystem.fromSerialized(
-        serializedKeys!,
+    hasKeys().then((walletHasKeys: boolean | null) => {
+      if (!walletHasKeys) {
+        // no keys, user should setup their wallet
+        return logUpdateState('READY')
+      }
+
+      hasPin().then((walletHasPin: boolean | null) =>
+        walletHasPin ? logUpdateState('LOCKED') : loadExistingWallets(),
       )
-
-      const rifWallets = await Promise.all(wallets.map(createRIFWallet))
-
-      const rifWalletsDictionary = rifWallets.reduce(
-        (p: Wallets, c: RIFWallet) => Object.assign(p, { [c.address]: c }),
-        {},
-      )
-
-      setKeys(kms, rifWalletsDictionary)
-    }
-
-    hasPin().then((answer: boolean | null) =>
-      setAppState(answer ? 'LOCKED' : 'READY'),
-    )
+    })
   }
 
   useEffect(() => {
-    console.log('starting init ;-)')
-    i18nInit().finally(init)
+    i18nInit().finally(appIsActive)
+
+    const subscription = AppState.addEventListener(
+      'change',
+      handleAppStateChange,
+    )
+
+    return () => {
+      subscription.remove()
+    }
   }, [])
 
-  if (appState === 'LOADING') {
-    return (
-      <SafeAreaView>
-        <Loading reason="Getting set..." />
-      </SafeAreaView>
-    )
-  }
+  const loadExistingWallets = async () => {
+    logUpdateState('LOADING')
+    const serializedKeys = await getKeys()
+    // eslint-disable-next-line no-shadow
+    const { kms, wallets } = KeyManagementSystem.fromSerialized(serializedKeys!)
 
-  if (appState === 'BACKGROUND') {
-    return <Cover />
-  }
+    const rifWallets = await Promise.all(wallets.map(createRIFWallet))
 
-  if (appState === 'LOCKED') {
-    return (
-      <SafeAreaView>
-        <RequestPIN unlock={() => setAppState('READY')} />
-      </SafeAreaView>
+    const rifWalletsDictionary = rifWallets.reduce(
+      (p: Wallets, c: RIFWallet) => Object.assign(p, { [c.address]: c }),
+      {},
     )
+
+    setKeys(kms, rifWalletsDictionary)
+    setAppState('READY')
   }
 
   const createFirstWallet = async (mnemonic: string) => {
@@ -133,7 +136,33 @@ const App = () => {
     return rifWallet
   }
 
+  const resetAppState = () => {
+    setKMS(null)
+    setWallets({})
+    setSelectedWallet('')
+  }
+
   const closeRequest = () => setRequests([] as Requests)
+
+  if (appState === 'LOADING') {
+    return (
+      <SafeAreaView>
+        <Loading reason="Getting set..." />
+      </SafeAreaView>
+    )
+  }
+
+  if (appState === 'BACKGROUND') {
+    return <Cover />
+  }
+
+  if (appState === 'LOCKED') {
+    return (
+      <SafeAreaView>
+        <RequestPIN unlock={loadExistingWallets} />
+      </SafeAreaView>
+    )
+  }
 
   return (
     <SafeAreaView>
