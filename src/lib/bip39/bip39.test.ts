97cef3f0b48ce7ff58a15ffeda08f514780067df
Luis Chavarría <luis.chavarria@iovlabs.org>
2021-12-06 19:10:22 -0600

    feat: add mnemonic validation

---
+++ src/lib/bip39/bip39.test.ts
@@ -0,0 +1,24 @@
+import { validateMnemonic } from './index'
+import {
+  VALID_SEED_PHRASE,
+  INVALID_WORD_SEED_PHRASE,
+  SEED_PHRASE_TOO_SHORT,
+} from './testCase'
+
+describe('Validate Mnemonic', () => {
+  beforeEach(async () => {})
+
+  test('mnemonic has invalid words', async () => {
+    const error = validateMnemonic(INVALID_WORD_SEED_PHRASE)
+    expect(error).toEqual('worldlist is not valid')
+  })
+  test('mnemonic is too short', async () => {
+    const error = validateMnemonic(SEED_PHRASE_TOO_SHORT)
+    expect(error).toEqual('you need to enter at least twelve words')
+  })
+
+  test('mnemonic is has valid words', async () => {
+    const error = validateMnemonic(VALID_SEED_PHRASE)
+    expect(error).toBeNull()
+  })
+})
---
+++ src/lib/bip39/testCase.ts
@@ -0,0 +1,5 @@
+export const VALID_SEED_PHRASE =
+  'huge gap dial bike human family often shove country maple sweet fresh project broken tube increase hat cement mammal inform powder shadow future axis'
+export const SEED_PHRASE_TOO_SHORT = 'huge gap dial bike human'
+export const INVALID_WORD_SEED_PHRASE =
+  'huge2 gap dial bike human family often shove country maple sweet fresh project broken tube increase hat cement mammal inform powder shadow future axis'
