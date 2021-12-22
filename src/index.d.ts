79ecb92c1d473e4d0549f300fa1ccc14851021e4
Luis Chavarría <luis.chavarria@iovlabs.org>
2021-12-22 07:55:04 -0600

    feat: add types

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,3 +6,52 @@ declare module '@rsksmart/rsk-utils' {
   ): boolean
   export function isAddress(address: string): boolean
 }
+
+declare module '@rsksmart/rns-sdk' {
+  import { Signer, BigNumber, ContractTransaction } from 'ethers'
+  export class RNS {
+    constructor(rnsRegistryAddress: string, signer: Signer)
+    setOwner(domain: string, owner: string): Promise<ContractTransaction>
+    getOwner(domain: string): Promise<string>
+    setResolver(domain: string, resolver: string): Promise<ContractTransaction>
+    getResolver(domain: string): Promise<string>
+    setSubdomainOwner(
+      domain: string,
+      label: string,
+      owner: string,
+    ): Promise<ContractTransaction>
+  }
+  export class AddrResolver {
+    constructor(rnsRegistryAddress: string, signer: Signer)
+    setAddr(domain: string, addr: string): Promise<ContractTransaction>
+    addr(domain: string): Promise<string>
+  }
+
+  export class RSKRegistrar {
+    constructor(
+      rskOwnerAddress: string,
+      fifsAddrRegistrarAddress: string,
+      rifTokenAddress: string,
+      signer: Signer,
+    )
+    available(label: string): Promise<string>
+    ownerOf(label: string): Promise<string>
+    price(label: string, duration: BigNumber): Promise<BigNumber>
+    commitToRegister(
+      label: string,
+      owner: string,
+    ): Promise<{
+      secret: string
+      makeCommitmentTransaction: ContractTransaction
+      canReveal: () => Promise<boolean>
+    }>
+    register(
+      label: string,
+      owner: string,
+      secret: string,
+      duration: BigNumber,
+      amount: BigNumber,
+      addr?: string,
+    ): Promise<ContractTransaction>
+  }
+}
