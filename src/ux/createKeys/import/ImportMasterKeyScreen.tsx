a617a55ae4deeb0a0f61efe090e0f79250bdff52
Agustin Villalobos <agustin.villalobos@iovlabs.org>
2022-01-05 10:13:18 -0600

    Removing hardcoded key

--- src/ux/createKeys/import/ImportMasterKeyScreen.tsx
+++ src/ux/createKeys/import/ImportMasterKeyScreen.tsx
@@ -12,9 +12,7 @@ type ImportMasterKeyScreenProps = {
 export const ImportMasterKeyScreen: React.FC<
   ScreenProps<'ImportMasterKey'> & ImportMasterKeyScreenProps
 > = ({ navigation, createFirstWallet }) => {
-  const [importMnemonic, setImportMnemonic] = useState<string>(
-    'toe among network melt ensure jaguar sleep toddler desert minute degree search that prosper detect recall story quantum scale wall power try replace knock',
-  )
+  const [importMnemonic, setImportMnemonic] = useState<string>('')
 
   const [error, setError] = useState<string | null>(null)
   const [info, setInfo] = useState<string | null>(null)
