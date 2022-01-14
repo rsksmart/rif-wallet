4ffaae303d3aa7d9cd9dd2efda76ba6ea42d7ffd
Agustin Villalobos <agustin.villalobos@iovlabs.org>
2022-01-14 10:49:34 -0600

    Implementing transaction info component and transaction error on the UI

--- src/screens/send/SendScreen.tsx
+++ src/screens/send/SendScreen.tsx
@@ -3,12 +3,10 @@ import LinearGradient from 'react-native-linear-gradient'
 import { useIsFocused } from '@react-navigation/native'
 
 import {
-  Dimensions,
   StyleSheet,
   View,
   TextInput,
   Linking,
-  Text,
   TouchableOpacity,
 } from 'react-native'
 
@@ -19,17 +17,17 @@ import { getAllTokens } from '../../lib/token/tokenMetadata'
 import { IToken } from '../../lib/token/BaseToken'
 
 import { ScreenProps } from '../../RootNavigation'
-import { Button, CopyComponent, Paragraph } from '../../components'
+import { Paragraph } from '../../components'
 import { ScreenWithWallet } from '../types'
 import { AddressInput } from '../../components'
 import Resolver from '@rsksmart/rns-resolver.js'
 import { getTokenColor, getTokenColorWithOpacity } from '../home/tokenColor'
 import { grid } from '../../styles/grid'
 import { SquareButton } from '../../components/button/SquareButton'
-import { Arrow, CompassIcon, CopyIcon } from '../../components/icons'
+import { Arrow, RefreshIcon } from '../../components/icons'
 import { TokenImage } from '../home/TokenImage'
-import { getAddressDisplayText } from '../../components'
 import Clipboard from '@react-native-community/clipboard'
+import TransactionInfo from './TransactionInfo'
 
 export type SendScreenProps = {
   rnsResolver: Resolver
@@ -51,10 +49,10 @@ export const SendScreen: React.FC<
   const [availableTokens, setAvailableTokens] = useState<IToken[]>()
   const [amount, setAmount] = useState('')
   const [tx, setTx] = useState<ContractReceipt | null>(null)
-  const [txConfirmed, setTxConfirmed] = useState(false)
   const [txSent, setTxSent] = useState(false)
   const [info, setInfo] = useState('')
   const [transferHash, setTransferHash] = useState<string | null>(null)
+  const [error, setError] = useState<string | null >(null)
 
   useEffect(() => {
     setTo(route.params?.to || '')
@@ -69,6 +67,7 @@ export const SendScreen: React.FC<
 
   const transfer = async (tokenSymbol: string) => {
     setInfo('')
+    setError(null)
     if (availableTokens) {
       const selectedSymbol = availableTokens.find(
         token => token.symbol === tokenSymbol,
@@ -87,14 +86,13 @@ export const SendScreen: React.FC<
           setInfo(t('Transaction Sent. Please wait...'))
 
           setTxSent(true)
-          setTxConfirmed(false)
           const txReceipt = await transferResponse.wait()
           setTransferHash(null)
           setTx(txReceipt)
           setInfo(t('Transaction Confirmed.'))
-          setTxConfirmed(true)
           // @ts-ignore
         } catch (e: any) {
+          setError(e.message)
           setInfo(t('Transaction Failed: ') + e.message)
         }
       }
@@ -124,13 +122,6 @@ export const SendScreen: React.FC<
   const handleCopy = () => Clipboard.setString(transferHash!)
   const handleOpen = () =>
     Linking.openURL(`https://explorer.testnet.rsk.co/tx/${transferHash}`)
-
-  const windowWidth = Dimensions.get('window').width
-  const qrCodeSize = windowWidth * 0.6
-  const qrContainerStyle = {
-    marginHorizontal: (windowWidth - (qrCodeSize + 20)) / 2,
-    width: qrCodeSize + 40,
-  }
 
   return (
     <LinearGradient
@@ -188,62 +179,32 @@ export const SendScreen: React.FC<
       <View style={styles.section}>
         <Paragraph>{info}</Paragraph>
         {transferHash && (
-          <React.Fragment>
-            <View style={{ ...styles.hashContainer, ...qrContainerStyle }}>
-              <Text style={styles.hash}>
-                {getAddressDisplayText(transferHash).displayAddress}
-              </Text>
-            </View>
-            <Text style={styles.hashLabel}>hash address</Text>
-
-            <View style={grid.row}>
-              <View style={{ ...grid.column6, ...styles.bottomColumn }}>
-                <SquareButton
-                  onPress={handleCopy}
-                  title="copy"
-                  testID="Hash.CopyButton"
-                  icon={
-                    <CopyIcon
-                      width={55}
-                      height={55}
-                      color={getTokenColor(selectedSymbol)}
-                    />
-                  }
-                />
-              </View>
-              <View style={{ ...grid.column6, ...styles.bottomColumn }}>
-                <SquareButton
-                  onPress={handleOpen}
-                  title="open"
-                  testID="Hash.OpenURLButton"
-                  icon={
-                    <CompassIcon
-                      width={30}
-                      height={30}
-                      color={getTokenColor(selectedSymbol)}
-                    />
-                  }
-                />
-              </View>
-            </View>
-          </React.Fragment>
-        )}
-      </View>
-      {txConfirmed && tx && (
-        <View testID={'TxReceipt.View'} style={styles.section}>
-          {tx && (
-            <CopyComponent value={tx.transactionHash} prefix={'Tx Hash: '} />
-          )}
-          <Button
-            title="View in explorer"
-            onPress={() => {
-              Linking.openURL(
-                `https://explorer.testnet.rsk.co/tx/${tx.transactionHash}`,
-              )
-            }}
+          <TransactionInfo
+            hash={transferHash}
+            selectedToken={selectedSymbol}
+            handleCopy={handleCopy}
+            handleOpen={handleOpen}
           />
-        </View>
+        )}
+        {!!tx && (
+        <TransactionInfo
+          hash={tx.transactionHash}
+          selectedToken={selectedSymbol}
+          handleCopy={handleCopy}
+          handleOpen={handleOpen}
+        />
       )}
+      {!!error && <View style={styles.centerRow}>
+      <SquareButton
+            onPress={async () => {
+              await transfer(selectedSymbol)
+            }}
+            title="Retry"
+            testID="Transfer.RetryButton"
+            icon={<RefreshIcon color={getTokenColor(selectedSymbol)}  />}
+          />
+      </View>}
+      </View>
     </LinearGradient>
   )
 }
@@ -291,25 +252,5 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#CCCCCC',
     flex: 1,
-  },
-  hash: {
-    color: '#5C5D5D',
-    fontSize: 16,
-    fontWeight: '500',
-    textAlign: 'center',
-  },
-  hashContainer: {
-    padding: 10,
-    borderRadius: 10,
-    backgroundColor: 'rgba(255, 255, 255, .7)',
-  },
-  hashLabel: {
-    color: '#5C5D5D',
-    textAlign: 'center',
-    marginTop: 5,
-    marginBottom: 20,
-  },
-  bottomColumn: {
-    alignItems: 'center',
-  },
+  }
 })
