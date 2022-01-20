023e372d9c76506e703e742e8743121e91ae88f4
Christian Escalante <chescalante@gmail.com>
2022-01-20 17:06:25 -0300

    WIP

--- src/lib/rifWalletServices/RifWalletServicesFetcher.ts
+++ src/lib/rifWalletServices/RifWalletServicesFetcher.ts
@@ -1,3 +1,4 @@
+import fetch from 'isomorphic-fetch'
 import {
   ITokenWithBalance,
   TransactionsServerResponse,
--- testLib/setupTests.ts
+++ testLib/setupTests.ts
@@ -1,3 +1,7 @@
+// import fetch from 'node-fetch'
+
+// global.fetch = fetch as any
+
 // Mock rn-secure-storage so that jest will not go looking for it.
 // eslint-disable-next-line no-undef
 jest.mock('rn-secure-storage', () => {})
