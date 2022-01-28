c6267137afd73d0fdb0a542509b4175b8f1cd887
Agustin Villalobos <agustin.villalobos@iovlabs.org>
2022-01-27 20:54:09 -0600

    Implementing top 5 enhanced transactions

--- src/screens/activity/ActivityScreen.tsx
+++ src/screens/activity/ActivityScreen.tsx
@@ -21,6 +21,7 @@ import ActivityRow from './ActivityRow'
 import { grid } from '../../styles/grid'
 import { SquareButton } from '../../components/button/SquareButton'
 import { Arrow, RefreshIcon } from '../../components/icons'
+import { IActivity } from '../../subscriptions/types'
 
 export interface IActivityTransaction {
   originTransaction: IApiTransaction
@@ -41,7 +42,10 @@ export const ActivityScreen: React.FC<
   ScreenProps<'Activity'> & ScreenWithWallet & ActivityScreenProps
 > = ({ wallet, fetcher, abiEnhancer, navigation }) => {
   const [info, setInfo] = useState('')
-  const { state, dispatch } = useSocketsState()
+  const {
+    state: { transactions },
+    dispatch,
+  } = useSocketsState()
   useState<TransactionsServerResponseWithActivityTransactions | null>(null)
   const { t } = useTranslation()
 
@@ -59,7 +63,7 @@ export const ActivityScreen: React.FC<
     /*i18n.changeLanguage('es')*/
     try {
       setInfo(t('Loading transactions. Please wait...'))
-      dispatch({ type: 'newActivity', payload: null })
+      //dispatch({ type: 'newActivity', payload: null })
 
       const fetchedTransactions: TransactionsServerResponseWithActivityTransactions =
         await fetcher.fetchTransactionsByAddress(
@@ -82,7 +86,11 @@ export const ActivityScreen: React.FC<
         }),
       )
 
-      dispatch({ type: 'newActivity', payload: fetchedTransactions })
+      dispatch({
+        type: 'newTransactions',
+        payload: fetchedTransactions as IActivity,
+      })
+      console.log(fetchedTransactions)
       setInfo('')
     } catch (e: any) {
       setInfo(t('Error reaching API: ') + e.message)
@@ -95,15 +103,13 @@ export const ActivityScreen: React.FC<
       <View style={{ ...grid.row, ...styles.refreshButtonView }}>
         <View style={{ ...grid.column4, ...styles.column }}>
           <SquareButton
-            onPress={() =>
-              fetchTransactionsPage({ prev: state.activities?.prev })
-            }
-            disabled={!state.activities?.prev}
+            onPress={() => fetchTransactionsPage({ prev: transactions?.prev })}
+            disabled={!transactions?.prev}
             title="prev"
             icon={
               <Arrow
                 rotate={270}
-                color={state.activities?.prev ? '#66777E' : '#f1f1f1'}
+                color={transactions?.prev ? '#66777E' : '#f1f1f1'}
               />
             }
           />
@@ -117,15 +123,13 @@ export const ActivityScreen: React.FC<
         </View>
         <View style={{ ...grid.column4, ...styles.column }}>
           <SquareButton
-            onPress={() =>
-              fetchTransactionsPage({ next: state.activities?.next })
-            }
-            disabled={!state.activities?.next}
+            onPress={() => fetchTransactionsPage({ next: transactions?.next })}
+            disabled={!transactions?.next}
             title="next"
             icon={
               <Arrow
                 rotate={90}
-                color={state.activities?.next ? '#66777E' : '#f1f1f1'}
+                color={transactions?.next ? '#66777E' : '#f1f1f1'}
               />
             }
           />
@@ -134,9 +138,9 @@ export const ActivityScreen: React.FC<
 
       {!!info && <Text testID="Info.Text">{info}</Text>}
 
-      {state.activities &&
-        state.activities.activityTransactions!.length > 0 &&
-        state.activities.activityTransactions!.map(
+      {transactions &&
+        transactions.activityTransactions!.length > 0 &&
+        transactions.activityTransactions!.map(
           (activityTransaction: IActivityTransaction) => (
             <ActivityRow
               key={activityTransaction.originTransaction.hash}
