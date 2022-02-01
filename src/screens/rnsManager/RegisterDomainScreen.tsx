384245feaf51846b8f78ff7ddba11b246315c10a
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-02-01 10:09:23 -0600

    feat: add new UI to RNS (#136)
    * feat: add new UI to RNS
    
    * Set autoCapitalize none
    
    * Add domain validation
    
    * Check if registered domains
    
    * Use 3 years by default + pass years to Register Domain screen
    
    * Show price in Register Domain screen + calculate it on duration change
    
    * Handle registration errors + realign registered domains + refactor domain store
    
    * Add RNS Manager to dapps
    
    * Lint
    
    Co-authored-by: Ilan <ilanolkies@outlook.com>

--- src/screens/rnsManager/RegisterDomainScreen.tsx
+++ src/screens/rnsManager/RegisterDomainScreen.tsx
@@ -1,24 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BigNumber } from 'ethers'
 import { StyleSheet, View, TextInput } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
 
-import { ScrollView, Text } from 'react-native'
+import { Text } from 'react-native'
 import addresses from './addresses.json'
 
 import { ScreenWithWallet } from '../types'
 import { Button } from '../../components'
 import { ScreenProps } from '../../RootNavigation'
 import { RSKRegistrar, AddrResolver } from '@rsksmart/rns-sdk'
-import { getDomains, saveDomains } from '../../storage/DomainsStore'
+import { addDomain } from '../../storage/DomainsStore'
+import { getTokenColorWithOpacity } from '../home/tokenColor'
+import { formatUnits } from 'ethers/lib/utils'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export const RegisterDomainScreen: React.FC<
-  ScreenProps<'Activity'> & ScreenWithWallet
+  ScreenProps<'RegisterDomain'> & ScreenWithWallet
 > = ({ wallet, route, navigation }) => {
+  const { selectedDomain, years } = route.params
+
+  const [error, setError] = useState('')
+
+  const [duration, setDuration] = useState(years.toString())
+  const [domainPrice, setDomainPrice] = useState<BigNumber>()
+
   const [commitToRegisterInfo, setCommitToRegisterInfo] = useState('')
-  const [registerDomainInfo, setRegisterDomainInfo] = useState('')
   const [domainSecret, setDomainSecret] = useState('')
-  const [duration, setDuration] = useState('1')
-  const [resolvingAddress, setResolvingAddress] = useState('1')
+  const [commitReady, setCommitReady] = useState(false)
+
+  const [registerDomainInfo, setRegisterDomainInfo] = useState('')
+  const [registerReady, setRegisterReady] = useState(false)
+
+  const [resolvingAddress, setResolvingAddress] = useState('')
+
   const rskRegistrar = new RSKRegistrar(
     addresses.rskOwnerAddress,
     addresses.fifsAddrRegistrarAddress,
@@ -26,109 +41,178 @@ export const RegisterDomainScreen: React.FC<
     wallet,
   )
   const addrResolver = new AddrResolver(addresses.rnsRegistryAddress, wallet)
-  const selectedDomain = route.params ?? ''
-  const commitToRegister = async (domain: string) => {
-    if (domain) {
+
+  useEffect(() => {
+    setDomainPrice(undefined)
+    if (duration) {
+      rskRegistrar
+        .price(selectedDomain, BigNumber.from(duration))
+        .then(price => setDomainPrice(price))
+    }
+  }, [duration])
+
+  const commitToRegister = async () => {
+    try {
       const { makeCommitmentTransaction, secret, canReveal } =
-        await rskRegistrar.commitToRegister(domain, wallet.smartWallet.address)
+        await rskRegistrar.commitToRegister(
+          selectedDomain,
+          wallet.smartWallet.address,
+        )
+
       setDomainSecret(secret)
       setCommitToRegisterInfo('Transaction sent. Please wait...')
+
       await makeCommitmentTransaction.wait()
 
-      setCommitToRegisterInfo('Almost done. Please wait approximately 2 mins')
-      setInterval(async () => {
+      setCommitToRegisterInfo(
+        'Transaction confirmed. Please wait approximately 2 mins to secure your domain',
+      )
+
+      const intervalId = setInterval(async () => {
         const ready = await canReveal()
         if (ready) {
-          setCommitToRegisterInfo('Done')
+          setCommitToRegisterInfo(
+            'Waiting period ended. You can now register your domain',
+          )
+          setCommitReady(true)
+          clearInterval(intervalId)
         }
       }, 5000)
-
-      //set timeout
+    } catch (e: any) {
+      setError(e.message)
     }
   }
+
   const registerDomain = async (domain: string) => {
-    const durationToRegister = BigNumber.from(duration)
-    const price = await rskRegistrar.price(domain, durationToRegister)
-    const tx = await rskRegistrar.register(
-      domain,
-      wallet.smartWallet.address,
-      domainSecret,
-      durationToRegister,
-      price,
-    )
-    setRegisterDomainInfo('Transaction sent. Please wait...')
-    const registrationReceipt = await tx.wait()
-    console.log(registrationReceipt)
-    setRegisterDomainInfo('Registered')
-    const domains = JSON.parse((await getDomains()) || '[]')
-    domains.push(`${selectedDomain}.rsk`)
-    await saveDomains(JSON.stringify(domains))
-    const address = await addrResolver.addr(selectedDomain)
-    setResolvingAddress(address)
+    try {
+      const durationToRegister = BigNumber.from(duration)
+
+      const tx = await rskRegistrar.register(
+        domain,
+        wallet.smartWallet.address,
+        domainSecret,
+        durationToRegister,
+        domainPrice!,
+      )
+
+      setRegisterDomainInfo('Transaction sent. Please wait...')
+
+      await tx.wait()
+
+      await addDomain(wallet.smartWalletAddress, `${selectedDomain}.rsk`)
+
+      const address = await addrResolver.addr(`${selectedDomain}.rsk`)
+      console.log({ address })
+      setResolvingAddress(address)
+
+      setRegisterDomainInfo('Registered!!')
+      setRegisterReady(true)
+    } catch (e: any) {
+      setError(e.message)
+    }
   }
 
   return (
-    <ScrollView>
-      <View style={styles.sectionCentered}>
-        <Text style={styles.title}>{selectedDomain}.rsk</Text>
-      </View>
+    <LinearGradient
+      colors={['#FFFFFF', getTokenColorWithOpacity('TRBTC', 0.1)]}
+      style={styles.parent}>
+      <ScrollView>
+        <View style={styles.sectionCentered}>
+          <Text style={styles.domainTitle}>{selectedDomain}.rsk</Text>
+        </View>
+        <View style={styles.sectionLeft}>
+          <Text>Registration process will require 3 steps:</Text>
+          <Text>1. First transaction: request to register the domain</Text>
+          <Text>
+            2. Secure domain: wait for aprox. 2 minutes to secure your
+            registration
+          </Text>
+          <Text>
+            3. Second transaction: registration! after this tx is confirmed you
+            will own your RNS domain
+          </Text>
+        </View>
 
-      <View style={styles.sectionCentered}>
-        <Text>1. Enter years</Text>
-        <TextInput
-          value={duration}
-          onChangeText={setDuration}
-          placeholder={'Years'}
-        />
-      </View>
+        {!!error && <Text style={styles.red}>{error}</Text>}
 
-      <View style={styles.sectionCentered}>
-        <Text>2. Request Domain</Text>
+        <View style={styles.sectionCentered}>
+          <Text>Year(s)</Text>
+          <TextInput
+            editable={commitToRegisterInfo === ''}
+            style={styles.input}
+            onChangeText={setDuration}
+            value={duration}
+            placeholder={''}
+          />
+          <Text>Price: {domainPrice ? formatUnits(domainPrice) : '...'}</Text>
+        </View>
 
-        {commitToRegisterInfo === '' ? (
+        <View style={styles.sectionCentered}>
           <Button
-            onPress={() => commitToRegister(selectedDomain)}
+            disabled={!duration || commitToRegisterInfo !== ''}
+            onPress={commitToRegister}
             title={'Request to register'}
           />
-        ) : null}
-        <Text>{commitToRegisterInfo}</Text>
-      </View>
-      <View style={styles.sectionCentered}>
-        <Text>3. Register Domain</Text>
-        {commitToRegisterInfo === 'Done' && registerDomainInfo === '' ? (
+
+          <Text>{commitToRegisterInfo}</Text>
+        </View>
+        <View style={styles.sectionCentered}>
           <Button
+            disabled={!commitReady || registerReady}
             onPress={() => registerDomain(selectedDomain)}
             title={'Register domain'}
           />
-        ) : null}
-        <Text>{registerDomainInfo}</Text>
-      </View>
-      {registerDomainInfo === 'Registered' ? (
-        <View style={styles.sectionCentered}>
-          <Text>You now own {selectedDomain}.rsk</Text>
-          <Text>Address: {resolvingAddress}</Text>
-          <Button
-            onPress={() => {
-              // @ts-ignore
-              navigation.navigate('Home')
-            }}
-            title={'Home'}
-          />
+
+          <Text>{registerDomainInfo}</Text>
         </View>
-      ) : null}
-    </ScrollView>
+        {registerReady && (
+          <View style={styles.sectionCentered}>
+            <Text>You now own {selectedDomain}.rsk</Text>
+            <Text>Address: {resolvingAddress}</Text>
+            <Button
+              onPress={() => {
+                // @ts-ignore
+                navigation.navigate('Home')
+              }}
+              title={'Home'}
+            />
+          </View>
+        )}
+      </ScrollView>
+    </LinearGradient>
   )
 }
 
 const styles = StyleSheet.create({
+  parent: {
+    height: '100%',
+    paddingTop: 20,
+  },
+  input: {
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    display: 'flex',
+    height: 50,
+    width: 40,
+    marginTop: 10,
+    margin: 10,
+    textAlign: 'center',
+  },
   sectionCentered: {
     paddingTop: 15,
     paddingBottom: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#CCCCCC',
     alignItems: 'center',
   },
-  title: {
+  domainTitle: {
     fontWeight: 'bold',
+    fontSize: 20,
+  },
+  sectionLeft: {
+    alignItems: 'flex-start',
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
+  },
+  red: {
+    color: 'red',
   },
 })
