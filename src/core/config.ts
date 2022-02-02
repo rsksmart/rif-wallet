4bcecc2da36a97c6098de04111024798bb0b07d2
Jesse Clark <hello@developerjesse.com>
2022-02-02 16:51:28 +0200

    Create json config files and connect setup to it.

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,4 +1,31 @@
-import { Platform } from 'react-native'
+import local from './config/local.json'
+import production from './config/production.json'
 
-export const rifWalletServicesUrl =
-  Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000' // 'https://rif-wallet-services.testnet.rifcomputing.net/'
+export enum SETTINGS {
+  RIF_WALLET_SERVICE_URL = 'rifWalletServicesUrl',
+  RPC_URL = 'rpcUrl',
+  SMART_WALLET_FACTORY_ADDRESS = 'SMART_WALLET_FACTORY_ADDRESS',
+}
+
+export enum ENVIRONMENTS {
+  LOCAL = 'local',
+  PRODUCTION = 'production',
+}
+
+export const getWalletSetting = (
+  setting: SETTINGS,
+  chainId: 31 = 31,
+): string => {
+  const env = ENVIRONMENTS.PRODUCTION
+  // @ts-expect-error
+  const configurationFile = env === ENVIRONMENTS.LOCAL ? local : production
+
+  switch (setting) {
+    case SETTINGS.RIF_WALLET_SERVICE_URL:
+      return configurationFile.rifWalletServicesUrl
+    case SETTINGS.RPC_URL:
+      return configurationFile.networks[chainId].rpcUrl
+    case SETTINGS.SMART_WALLET_FACTORY_ADDRESS:
+      return configurationFile.networks[chainId].smartWalletFactoryAddress
+  }
+}
