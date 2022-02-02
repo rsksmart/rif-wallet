fcc1bf36e3909930322a2722b3a66bf88f958628
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-02-01 21:41:25 -0600

    chore: add comment

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,4 +1,5 @@
 import { Platform } from 'react-native'
 
+//Dont include trailing slash
 export const rifWalletServicesUrl =
   Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000' // 'https://rif-wallet-services.testnet.rifcomputing.net'
