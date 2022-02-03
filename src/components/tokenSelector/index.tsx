0aebadc216869d89da29de2b91a100f8a7210a4c
Agustin Villalobos <agustin.villalobos@iovlabs.org>
2022-02-03 17:54:02 -0600

    Adding token image component to button as prop

--- src/components/tokenSelector/index.tsx
+++ src/components/tokenSelector/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useSocketsState } from '../../subscriptions/RIFSockets'
 import { ScrollView, View } from 'react-native'
 import { Button } from '../button'
 import { IToken } from '../../lib/token/BaseToken'
 
 import { sharedStyles } from '../../ux/requestsModal/sharedStyles'
 import { ModalHeader } from '../index'
+import { balanceToString } from '../../screens/balances/BalancesScreen'
+import { TokenImage } from '../../screens/home/TokenImage'
 
 interface Interface {
   availableTokens: IToken[]
@@ -15,19 +18,32 @@ const TokenSelector: React.FC<Interface> = ({
   availableTokens,
   onTokenSelection,
 }) => {
+  const {
+    state: { balances },
+  } = useSocketsState()
+
   return (
     <ScrollView>
       <View style={[sharedStyles.modalView, sharedStyles.modalViewMainSection]}>
         <ModalHeader>Select Token</ModalHeader>
 
-        {availableTokens.map((token: any) => (
-          <View key={token.symbol}>
-            <Button
-              onPress={() => onTokenSelection(token.symbol)}
-              title={token.symbol}
-            />
-          </View>
-        ))}
+        {availableTokens.map((token: IToken) => {
+          const balance = balanceToString(
+            balances[token.address].balance,
+            balances[token.address].decimals,
+          )
+          const Icon = <TokenImage symbol={balances[token.address].symbol} />
+          return (
+            <View key={token.symbol}>
+              <Button
+                onPress={() => onTokenSelection(token.symbol)}
+                title={token.symbol}
+                balance={balance}
+                icon={<TokenImage symbol={balances[token.address].symbol} />}
+              />
+            </View>
+          )
+        })}
       </View>
     </ScrollView>
   )
