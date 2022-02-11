3b68368a95a382840bb5b3a65c514136f50084bd
Ilan <36084092+ilanolkies@users.noreply.github.com>
2022-02-11 11:09:22 -0300

    Update package.json (#153)
    * Update package.json
    
    * Lint

--- src/screens/balances/BalancesScreen.test.tsx
+++ src/screens/balances/BalancesScreen.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react'
 import { fireEvent, render, waitFor } from '@testing-library/react-native'
 import { BalancesScreen, balanceToString } from './BalancesScreen'
