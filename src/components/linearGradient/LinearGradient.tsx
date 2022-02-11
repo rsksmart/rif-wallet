f4345b3204011dd09db8e36ab815b2c092eb1ba1
Ilan <ilanolkies@outlook.com>
2022-02-11 13:13:44 -0300

    Remove react-native-linear-gradient

--- src/components/linearGradient/LinearGradient.tsx
+++ src/components/linearGradient/LinearGradient.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import RNLinearGradient from 'react-native-linear-gradient'
+import { View } from 'react-native'
+// import RNLinearGradient from 'react-native-linear-gradient'
 
 export const LinearGradient: React.FC<{
   style: any,
   colors: any
-}> = ({ style, colors, children }) => <RNLinearGradient style={style} colors={colors}>
+}> = ({ style, colors, children }) => <View style={style}>
   {children}
-</RNLinearGradient>
+</View>
