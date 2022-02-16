37b3943b706bce3f70d3f9b580dea7f613fb6169
Jesse Clark <hello@developerjesse.com>
2022-02-16 14:59:21 +0200

    Add a colors const.

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  green: '#71F5AE',
+  orange: '#F89F84',
+
+  blue: '#6053F8',
+  blueLight: '#DAE2FF', // #DBE3FF
+  blueDark: '#002653', // #050134
+
+  gray: '#707070',
+  black: '#000000',
+  white: '#FFFFFF',
+
+  purpleLight: '#A2A3E5',
+  purple: '#74709B',
+  purpleDark: '#050134',
+}
