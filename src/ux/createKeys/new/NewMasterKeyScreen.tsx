af7a8888dd83dfdbc6bf8253616fd56bb9afeab0
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-02-22 08:46:07 -0600

    fix: refactor see 24 words screen

--- src/ux/createKeys/new/NewMasterKeyScreen.tsx
+++ src/ux/createKeys/new/NewMasterKeyScreen.tsx
@@ -1,95 +1,87 @@
-import React, { useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 import { StyleSheet, View, ScrollView, Text } from 'react-native'
-import { CopyComponent } from '../../../components'
 import { CreateKeysProps, ScreenProps } from '../types'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import LinearGradient from 'react-native-linear-gradient'
 
-import {
-  getTokenColor,
-  getTokenColorWithOpacity,
-  setOpacity,
-} from '../../../screens/home/tokenColor'
+import { getTokenColor } from '../../../screens/home/tokenColor'
 import { SquareButton } from '../../../components/button/SquareButton'
 import { Arrow } from '../../../components/icons'
-import balances from '../../../screens/home/tempBalances.json'
-import { ITokenWithBalance } from '../../../lib/rifWalletServices/RIFWalletServicesTypes'
 type CreateMasterKeyScreenProps = {
   generateMnemonic: CreateKeysProps['generateMnemonic']
 }
+import { grid } from '../../../styles/grid'
+
+const Word = ({ index, text }: { index: number; text: string }) => (
+  <View
+    style={{
+      ...grid.column4,
+      ...styles.wordContainer,
+    }}>
+    <Text style={styles.wordIndex}>{index}. </Text>
+    <Text style={styles.wordContent}>{text}</Text>
+  </View>
+)
 
 export const NewMasterKeyScreen: React.FC<
   ScreenProps<'NewMasterKey'> & CreateMasterKeyScreenProps
 > = ({ navigation, generateMnemonic }) => {
   const mnemonic = useMemo(generateMnemonic, [])
-  const [selected] = useState<ITokenWithBalance>(
-    balances[0] as ITokenWithBalance,
-  )
-
-  const selectedTokenColor = getTokenColor(selected.symbol)
-
-  const containerStyles = {
-    shadowColor: setOpacity(selectedTokenColor, 0.5),
-  }
-  const selectedToken = 'TRBTC'
+  const words = mnemonic.split(' ')
+  const rows = [1, 2, 3, 4, 5, 6, 7, 8]
 
   return (
-    <LinearGradient
-      colors={['#FFFFFF', getTokenColorWithOpacity('TRBTC', 0.1)]}
-      style={styles.parent}>
-      <Text style={styles.header}>Master Key</Text>
-      <LinearGradient
-        colors={['#FFFFFF', '#E1E1E1']}
-        style={{ ...styles.topContainer, ...containerStyles }}>
-        <ScrollView style={styles.portfolio}>
-          <TouchableOpacity>
-            <CopyComponent testID={'mnemonic'} value={mnemonic} />
-          </TouchableOpacity>
-        </ScrollView>
-      </LinearGradient>
-      <View style={styles.centerRow}>
-        <SquareButton
-          // @ts-ignore
-          onPress={() => navigation.navigate('ImportMasterKey', { mnemonic })}
-          title="Continue"
-          testID="Address.CopyButton"
-          icon={<Arrow color={getTokenColor(selectedToken)} rotate={90} />}
-        />
-      </View>
-    </LinearGradient>
+    <ScrollView style={styles.parent}>
+      <Text style={styles.header}>Write down your master key</Text>
+
+      {rows.map((row, i) => (
+        <View style={grid.row}>
+          <Word index={row} text={words[i]} />
+          <Word index={row + rows.length} text={words[i + rows.length]} />
+          <Word
+            index={row + rows.length * 2}
+            text={words[i + 16] + rows.length * 2}
+          />
+        </View>
+      ))}
+      {/*TODO:This button will be remove when the navigation is implemented*/}
+      <SquareButton
+        // @ts-ignore
+        onPress={() => navigation.navigate('ImportMasterKey', { mnemonic })}
+        title="Continue"
+        testID="Address.CopyButton"
+        icon={<Arrow color={getTokenColor('RBTC')} rotate={90} />}
+      />
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
-  centerRow: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   parent: {
-    height: '100%',
+    backgroundColor: '#050134',
   },
   header: {
-    fontSize: 26,
+    color: '#ffffff',
+    fontSize: 22,
+    paddingVertical: 20,
     textAlign: 'center',
   },
-  secHeader: {
-    fontSize: 18,
-    textAlign: 'center',
+  wordContent: {
+    backgroundColor: 'rgba(219, 227, 255, 0.3)',
+    color: '#ffffff',
+    borderRadius: 30,
+    justifyContent: 'flex-start',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
   },
-
-  portfolio: {
-    backgroundColor: '#ffffff',
-    paddingHorizontal: 25,
-    borderRadius: 25,
-    padding: 30,
+  wordIndex: {
+    color: '#ffffff',
+    display: 'flex',
+    paddingVertical: 5,
   },
-  topContainer: {
-    marginTop: 60,
-    marginHorizontal: 25,
-    borderRadius: 25,
-    backgroundColor: '#ffffff',
-    shadowOpacity: 0.1,
-    // shadowRadius: 10,
-    elevation: 2,
+  wordContainer: {
+    alignItems: 'flex-start',
+    color: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 5,
   },
 })
