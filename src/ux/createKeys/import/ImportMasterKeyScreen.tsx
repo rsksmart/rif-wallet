0f9e44029b42748f656d42f957b3fcfb7b46db4e
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-03-06 16:56:05 -0600

    feat: UI for import with 24 words (typing them with keyboard) (#171)
    * feat: UI for import with 24 words (typing them with keyboard)
    
    * feat: UI for import with 24 words (typing them with keyboard)
    
    * feat: UI for import with 24 words (typing them with keyboard)

--- src/ux/createKeys/import/ImportMasterKeyScreen.tsx
+++ src/ux/createKeys/import/ImportMasterKeyScreen.tsx
@@ -1,17 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, View, ScrollView, TextInput, Text } from 'react-native'
-import LinearGradient from 'react-native-linear-gradient'
+import React, { useState } from 'react'
+import { StyleSheet, View, ScrollView, Text } from 'react-native'
 
-import { Paragraph } from '../../../components'
+import { CreateKeysProps, ScreenProps } from '../types'
+import { grid } from '../../../styles/grid'
 import { validateMnemonic } from '../../../lib/bip39'
-import { ScreenProps, CreateKeysProps } from '../types'
-import {
-  getTokenColor,
-  getTokenColorWithOpacity,
-  setOpacity,
-} from '../../../screens/home/tokenColor'
 import { SquareButton } from '../../../components/button/SquareButton'
 import { Arrow } from '../../../components/icons'
+import { colors } from '../../../styles/colors'
+import { WordInput } from './WordInput'
 
 type ImportMasterKeyScreenProps = {
   createFirstWallet: CreateKeysProps['createFirstWallet']
@@ -19,28 +15,25 @@ type ImportMasterKeyScreenProps = {
 
 export const ImportMasterKeyScreen: React.FC<
   ScreenProps<'ImportMasterKey'> & ImportMasterKeyScreenProps
-> = ({ navigation, createFirstWallet, route }) => {
-  const [importMnemonic, setImportMnemonic] = useState<string>('')
+> = ({ navigation, createFirstWallet }) => {
+  const [selectedWords, setSelectedWords] = useState<string[]>([])
 
-  useEffect(() => {
-    // @ts-ignore
-    if (route.params?.mnemonic) {
-      // @ts-ignore
-      setImportMnemonic(route.params.mnemonic)
-    }
-  }, [])
-
+  const rows = [1, 2, 3, 4, 5, 6, 7, 8]
+  const onWordSelected = (selectedWord: string, wordNumber: number) => {
+    const currentWordsSelections = selectedWords
+    currentWordsSelections.splice(wordNumber - 1, 0, selectedWord)
+    setSelectedWords(currentWordsSelections)
+  }
   const [error, setError] = useState<string | null>(null)
   const [info, setInfo] = useState<string | null>(null)
-
   const handleImportMnemonic = async () => {
-    const mnemonicError = validateMnemonic(importMnemonic)
+    const mnemonicError = validateMnemonic(selectedWords.join(' '))
     if (!mnemonicError) {
       try {
         setInfo('Creating...')
-        const rifWallet = await createFirstWallet(importMnemonic)
+        const rifWallet = await createFirstWallet(selectedWords.join(' '))
         setInfo(null)
-        // @ts-ignore
+        console.log(rifWallet.address)
         navigation.navigate('KeysCreated', { address: rifWallet.address })
       } catch (err) {
         console.error(err)
@@ -51,99 +44,51 @@ export const ImportMasterKeyScreen: React.FC<
     }
     setError(mnemonicError)
   }
-  const setText = (text: string) => {
-    setError(null)
-    setImportMnemonic(text)
-  }
-  const selectedToken = 'TRBTC'
-
-  const selectedTokenColor = getTokenColor(selectedToken)
-
-  const containerStyles = {
-    shadowColor: setOpacity(selectedTokenColor, 0.5),
-  }
   return (
-    <LinearGradient
-      colors={['#FFFFFF', getTokenColorWithOpacity('TRBTC', 0.1)]}
-      style={styles.parent}>
-      <Text style={styles.header}>Master Key</Text>
-      <LinearGradient
-        colors={['#FFFFFF', '#E1E1E1']}
-        style={{ ...styles.topContainer, ...containerStyles }}>
-        <ScrollView style={styles.portfolio}>
-          <TextInput
-            onChangeText={text => setText(text)}
-            value={importMnemonic}
-            placeholder="Enter your 12 words master key"
-            multiline
+    <ScrollView style={styles.parent}>
+      <Text style={styles.header}>Enter your master key</Text>
+
+      {rows.map(row => (
+        <View style={grid.row} key={row}>
+          <WordInput wordNumber={row} handleSelection={onWordSelected} />
+          <WordInput
+            wordNumber={row + rows.length}
+            handleSelection={onWordSelected}
           />
-        </ScrollView>
-      </LinearGradient>
-      {info && <Paragraph>{info}</Paragraph>}
-      {error && <Paragraph>{error}</Paragraph>}
-      <View style={styles.centerRow}>
+          <WordInput
+            wordNumber={row + rows.length * 2}
+            handleSelection={onWordSelected}
+          />
+        </View>
+      ))}
+      <Text style={styles.defaultText}> {selectedWords.join(' ')}</Text>
+      <View>
+        {info && <Text style={styles.defaultText}>{info}</Text>}
+        {error && <Text style={styles.defaultText}> {error}</Text>}
+
         <SquareButton
-          onPress={() => handleImportMnemonic()}
-          title="Confirm"
+          onPress={handleImportMnemonic}
+          title=""
           testID="Address.CopyButton"
-          icon={<Arrow color={getTokenColor(selectedToken)} rotate={90} />}
+          icon={<Arrow color={colors.gray} rotate={90} />}
         />
       </View>
-    </LinearGradient>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
-  centerRow: {
-    justifyContent: 'center',
-    alignItems: 'center',
+  defaultText: {
+    color: colors.white,
   },
   parent: {
-    height: '100%',
-  },
-  header: {
-    fontSize: 26,
-    textAlign: 'center',
-  },
-  section: {
-    paddingTop: 15,
-    paddingBottom: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#CCCCCC',
-  },
-  sectionCentered: {
-    paddingTop: 15,
-    paddingBottom: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#CCCCCC',
-    alignItems: 'center',
-  },
-  input: {
-    height: 80,
-    margin: 12,
-    borderWidth: 1,
-    borderColor: '#000',
-    padding: 10,
-    textAlignVertical: 'top',
-  },
-  secHeader: {
-    fontSize: 18,
-    textAlign: 'center',
+    backgroundColor: colors.darkBlue,
   },
 
-  portfolio: {
-    backgroundColor: '#ffffff',
-    paddingHorizontal: 25,
-    borderRadius: 25,
-    padding: 30,
-  },
-  topContainer: {
-    marginTop: 60,
-    marginHorizontal: 25,
-    borderRadius: 25,
-    backgroundColor: '#ffffff',
-    shadowOpacity: 0.1,
-    // shadowRadius: 10,
-    elevation: 2,
+  header: {
+    color: '#ffffff',
+    fontSize: 22,
+    paddingVertical: 20,
+    textAlign: 'center',
   },
 })
