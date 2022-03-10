6ae57b9b0662aea46024bf6e68e656c248325d61
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-03-09 22:59:33 -0600

    feat: refactor manage ping screen

--- src/screens/managePin/ManagePinScreen.tsx
+++ src/screens/managePin/ManagePinScreen.tsx
@@ -1,78 +1,127 @@
-import React, { useState, useEffect } from 'react'
-import { View, ScrollView, StyleSheet, Text } from 'react-native'
-import { Button, CustomInput } from '../../components'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { getPin, savePin } from '../../storage/PinStore'
+
+import { shareStyles } from '../../components/sharedStyles'
 import { Trans, useTranslation } from 'react-i18next'
-import { hasPin, savePin, removePin } from '../../storage/PinStore'
+import { KeyPad } from '../../components/keyPad'
+import { colors } from '../../styles/colors'
+import { validateMnemonic } from '../../lib/bip39'
 
-export const ManagePinScreen = () => {
-  const [pin, setPin] = useState('')
-  const [pinSaved, setPinSaved] = useState(false)
-  useEffect(() => {
-    const callStorage = async () => {
-      const pinSet = await hasPin()
-      setPinSaved(pinSet || false)
-      return pinSet
-    }
+interface Interface {
+  unlock: () => void
+  route: any
+}
 
-    callStorage().then(pinSet => console.log(pinSet))
-  }, [])
+export const ManagePinScreen: React.FC<Interface> = ({ unlock, route }) => {
+  const mnemonic = route.params.address
+
+  const [error, setError] = useState<string | null>(null)
+  const [pin, setPin] = React.useState<Array<string>>(['', '', '', ''])
+  const [storedPin, setStoredPin] = React.useState<string | null>('')
+  const [position, setPosition] = React.useState(0)
 
   const { t } = useTranslation()
 
-  const saveMyPin = (newPin: string) => {
-    setPinSaved(true)
-    savePin(newPin)
+  const storePin = (enteredPin: string) => () => {
+    setError(null)
+    savePin(enteredPin).then(() => {
+      setError('pin saved')
+      setPosition(4)
+      setStoredPin(enteredPin)
+      const rifWallet = await createFirstWallet(mnemonic)
+    })
   }
-  const removeMyPin = () => {
-    setPinSaved(false)
-    removePin()
+
+  const onPressKey = (value: string) => {
+    setPin(prev => {
+      if (position < 4) {
+        const tempPin = [...pin]
+        tempPin.splice(position, 1, value)
+        setPosition(position + 1)
+        return tempPin
+      }
+      return prev
+    })
   }
+
+  const onDelete = () => {
+    setError(null)
+    setPin(prev => {
+      if (position > 0) {
+        const tempPin = [...pin]
+        tempPin.splice(position - 1, 1, '')
+        setPosition(position - 1)
+        return tempPin
+      }
+      return prev
+    })
+  }
+  getPin().then(retrievedPin => {
+    setStoredPin(retrievedPin)
+  })
+
   return (
-    <ScrollView>
-      {pinSaved && (
-        <View style={styles.container}>
-          <Text style={{ ...styles.header, ...styles.marginBottom }}>
-            <Trans>Your pin is set</Trans>
-          </Text>
-          <Button onPress={() => removeMyPin()} title={t('Delete your Pin')} />
-        </View>
+    <View
+      style={{
+        ...shareStyles.coverAllScreen,
+        ...styles.container,
+      }}>
+      <Text style={styles.header}>
+        {!storedPin && <Trans>{t('Set your PIN')}</Trans>}
+        {storedPin && <Trans>{t('Your pin is set')}</Trans>}
+      </Text>
+      <View style={styles.dotsWrapper}>
+        {pin.map((digit, index) => (
+          <View
+            key={`${digit}${index}`}
+            style={{ ...styles.dot, ...(!!digit && styles.filledDot) }}
+          />
+        ))}
+      </View>
+      {error && <Text style={styles.error}>{error}</Text>}
+      {!storedPin && (
+        <KeyPad
+          onDelete={onDelete}
+          onKeyPress={onPressKey}
+          onUnlock={storePin(pin.join(''))}
+        />
       )}
-
-      {!pinSaved && (
-        <View style={styles.container}>
-          <Text style={{ ...styles.header, ...styles.marginBottom }}>
-            <Trans>Set your pin</Trans>
-          </Text>
-          <View style={styles.marginBottom}>
-            <CustomInput
-              onChange={setPin}
-              placeholder={t('Pin')}
-              testID={'To.Input'}
-              keyboardType="numeric"
-            />
-          </View>
-
-          <View>
-            <Button
-              onPress={() => saveMyPin(pin)}
-              title={t('Save')}
-              testID="Next.Button"
-            />
-          </View>
-        </View>
-      )}
-    </ScrollView>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 25,
-    marginTop: 30,
-  },
-  marginBottom: { marginBottom: 10 },
   header: {
-    fontSize: 26,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#FFFFFF',
+    marginBottom: 20,
+    marginTop: 35,
+  },
+  container: {
+    paddingHorizontal: 60,
+    paddingTop: 60,
+    backgroundColor: colors.darkPurple3,
+  },
+  dot: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    borderRadius: 40,
+    height: 30,
+    marginRight: 10,
+    width: 30,
+  },
+  filledDot: {
+    backgroundColor: colors.lightPurple,
+  },
+  dotsWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 40,
+  },
+  error: {
+    color: '#FFFFFF',
     textAlign: 'center',
   },
 })
