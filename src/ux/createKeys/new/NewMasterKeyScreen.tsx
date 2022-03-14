ada24d2a27bea884b26bde542886c2864026b768
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-03-13 20:20:52 -0600

    feat: style navigation footer and reset button (#177)
    * feat: style navigatio footer and reset button
    
    * feat: style navigatio footer and reset button

--- src/ux/createKeys/new/NewMasterKeyScreen.tsx
+++ src/ux/createKeys/new/NewMasterKeyScreen.tsx
@@ -4,14 +4,12 @@ import { CreateKeysProps, ScreenProps } from '../types'
 import { Trans } from 'react-i18next'
 import { colors } from '../../../styles/colors'
 
-import { getTokenColor } from '../../../screens/home/tokenColor'
-import { SquareButton } from '../../../components/button/SquareButton'
-import { Arrow } from '../../../components/icons'
 type CreateMasterKeyScreenProps = {
   generateMnemonic: CreateKeysProps['generateMnemonic']
 }
 import { grid } from '../../../styles/grid'
 import { Word } from './Word'
+import { NavigationFooter } from '../../../components/button/NavigationFooter'
 
 export const NewMasterKeyScreen: React.FC<
   ScreenProps<'NewMasterKey'> & CreateMasterKeyScreenProps
@@ -21,33 +19,32 @@ export const NewMasterKeyScreen: React.FC<
   const rows = [0, 1, 2, 3, 4, 5, 6, 7]
 
   return (
-    <ScrollView style={styles.parent}>
-      <Text style={styles.header}>
-        <Trans>Write down your master key</Trans>
-      </Text>
+    <>
+      <ScrollView style={styles.parent}>
+        <Text style={styles.header}>
+          <Trans>Write down your master key</Trans>
+        </Text>
 
-      {rows.map(row => (
-        <View style={grid.row} key={row}>
-          <Word wordNumber={row + 1} text={words[row]} />
-          <Word
-            wordNumber={row + 1 + rows.length}
-            text={words[row + rows.length]}
-          />
-          <Word
-            wordNumber={row + 1 + rows.length * 2}
-            text={words[row + rows.length * 2]}
-          />
-        </View>
-      ))}
-      {/*TODO:This button will be remove when the navigation is implemented*/}
-      <SquareButton
-        // @ts-ignore
+        {rows.map(row => (
+          <View style={grid.row} key={row}>
+            <Word wordNumber={row + 1} text={words[row]} />
+            <Word
+              wordNumber={row + 1 + rows.length}
+              text={words[row + rows.length]}
+            />
+            <Word
+              wordNumber={row + 1 + rows.length * 2}
+              text={words[row + rows.length * 2]}
+            />
+          </View>
+        ))}
+      </ScrollView>
+      <NavigationFooter
+        onBackwards={() => navigation.navigate('CreateKeys')}
         onPress={() => navigation.navigate('ConfirmNewMasterKey', { mnemonic })}
-        title=""
-        testID="Address.CopyButton"
-        icon={<Arrow color={getTokenColor('RBTC')} rotate={90} />}
+        title="continue"
       />
-    </ScrollView>
+    </>
   )
 }
 
@@ -62,13 +59,5 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     textAlign: 'center',
     fontWeight: 'bold',
-  },
-  wordContent: {
-    borderRadius: 30,
-    backgroundColor: colors.darkPurple2,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    paddingHorizontal: 10,
-    paddingVertical: 7,
   },
 })
