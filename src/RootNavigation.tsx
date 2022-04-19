fa235e840ba94b39c6554033edd8049eb8ca81e9
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-04-18 23:29:23 -0600

    fix: add type

--- src/RootNavigation.tsx
+++ src/RootNavigation.tsx
@@ -99,7 +99,7 @@ export const RootNavigation: React.FC<{
   hasPin: boolean
   rifWalletServicesSocket: IRifWalletServicesSocket
   keyManagementProps: CreateKeysProps
-  handlePinCreated: any
+  handlePinCreated: () => Promise<void>
   balancesScreenProps: BalancesScreenProps
   activityScreenProps: ActivityScreenProps
   keysInfoScreenProps: KeysInfoScreenProps
