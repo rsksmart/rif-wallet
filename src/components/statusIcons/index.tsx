9a58bd507e40ac8fdb28af5e8bff1b079627173f
Francis Rodriguez <francisngg@gmail.com>
2022-06-08 12:00:23 -0400

    StatusIcons will have a props override

--- src/components/statusIcons/index.tsx
+++ src/components/statusIcons/index.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 
 type Props = {
   status: string
+  props?: any
 }
 
 export const STATUS_MAP = {
@@ -13,14 +14,14 @@ export const STATUS_MAP = {
   FAILED: 'FAILED',
 }
 
-const StatusIcon = ({ status }: Props) => {
+const StatusIcon = ({ status, ...props }: Props) => {
   switch (status.toUpperCase()) {
     case STATUS_MAP.SUCCESS:
-      return <CheckIcon color="green" width={30} height={30} />
+      return <CheckIcon color="green" width={30} height={30} {...props} />
     case STATUS_MAP.FAILED:
-      return <DeleteIcon color="red" width={30} height={30} />
+      return <DeleteIcon color="red" width={30} height={30} {...props} />
     case STATUS_MAP.PENDING:
-      return <Text>Pending</Text>
+      return <Text {...props}>Pending</Text>
     default:
       throw new Error(`Tried to use a non-existent status: ${status} `)
   }
