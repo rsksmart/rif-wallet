4fb7d4affcd82eee3687439e6fc047bbd52ad1dd
Francis Rodriguez <39339295+Freshenext@users.noreply.github.com>
2022-06-10 10:05:12 -0400

    Added status icon component (#216)

---
+++ src/components/statusIcons/index.tsx
@@ -0,0 +1,30 @@
+import { Text } from 'react-native'
+import { CheckIcon } from '../icons/CheckIcon'
+import DeleteIcon from '../icons/DeleteIcon'
+import React from 'react'
+
+type Props = {
+  status: string
+  [key: string]: any
+}
+
+export const STATUS_MAP = {
+  SUCCESS: 'SUCCESS',
+  FAILED: 'FAILED',
+  PENDING: 'PENDING',
+}
+
+const StatusIcon = ({ status, ...props }: Props) => {
+  switch (status.toUpperCase()) {
+    case STATUS_MAP.SUCCESS:
+      return <CheckIcon color="green" width={30} height={30} {...props} />
+    case STATUS_MAP.FAILED:
+      return <DeleteIcon color="red" width={30} height={30} {...props} />
+    case STATUS_MAP.PENDING:
+      return <Text {...props}>Pending</Text>
+    default:
+      throw new Error(`Tried to use a non-existent status: ${status} `)
+  }
+}
+
+export default StatusIcon
