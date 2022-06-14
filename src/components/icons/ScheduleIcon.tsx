8cc21515cadac2c131b256a9fcd6a9e67f8b57eb
Francis Rodriguez <francisngg@gmail.com>
2022-06-14 12:09:51 -0400

    Added material UI icons and one as an example for Schedule

---
+++ src/components/icons/ScheduleIcon.tsx
@@ -0,0 +1,9 @@
+// @ts-ignore
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+import React from 'react'
+
+const ScheduleIcon = (props?: object) => (
+  <MaterialIcon name="schedule" {...props} />
+)
+
+export default ScheduleIcon
