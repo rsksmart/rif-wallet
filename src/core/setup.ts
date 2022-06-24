4167ff081b7b67d09979d4457216f007d2f4788a
Sleyter Sandoval <sleyter.sandoval@iovlabs.org>
2022-06-24 01:34:42 -0500

    Separating types

--- src/core/setup.ts
+++ src/core/setup.ts
@@ -6,6 +6,7 @@ import { AbiEnhancer } from '../lib/abiEnhancer/AbiEnhancer'
 import { getWalletSetting, SETTINGS } from './config'
 import { RifWalletServicesSocket } from '../lib/rifWalletServices/RifWalletServicesSocket'
 import { RifRelayService } from '../lib/rifRelayService/RifRelayService'
+import { RifRelayConfiguration } from '../lib/rifRelayService/types'
 
 export const networkId = 31
 
@@ -28,16 +29,20 @@ export const rifWalletServicesSocket = new RifWalletServicesSocket(
   abiEnhancer,
 )
 
+const rifRelayConfig: RifRelayConfiguration = {
+  preferedRelays: ['https://dev.relay.rifcomputing.net:8090'],
+  relayHubAddress: '0x66Fa9FEAfB8Db66Fe2160ca7aEAc7FC24e254387',
+  relayVerifierAddress: '0x56ccdB6D312307Db7A4847c3Ea8Ce2449e9B79e9',
+  deployVerifierAddress: '0x5C6e96a84271AC19974C3e99d6c4bE4318BfE483',
+  smartWalletAddress: '0xEdB6D515C2DB4F9C3C87D7f6Cefb260B3DEe8014',
+  relayWorkerAddress:'0xc6a4f4839b074b2a75ebf00a9b427ccb8073b7b4',
+  smartWalletFactoryAddress:smartWalletFactoryAddress,
+  testTokenAddress:'0xF5859303f76596dD558B438b18d0Ce0e1660F3ea',
+  rskHost:rpcUrl
+}
+
 export const rifRelayService = new RifRelayService(
-  ['https://dev.relay.rifcomputing.net:8090'],
-  '0x66Fa9FEAfB8Db66Fe2160ca7aEAc7FC24e254387',
-  '0x56ccdB6D312307Db7A4847c3Ea8Ce2449e9B79e9',
-  '0x5C6e96a84271AC19974C3e99d6c4bE4318BfE483',
-  '0xEdB6D515C2DB4F9C3C87D7f6Cefb260B3DEe8014',
-  '0xc6a4f4839b074b2a75ebf00a9b427ccb8073b7b4',
-  smartWalletFactoryAddress,
-  '0xF5859303f76596dD558B438b18d0Ce0e1660F3ea',
-  rpcUrl,
+  rifRelayConfig
 )
 
 export const rnsResolver = Resolver.forRskTestnet({})
---
+++ src/lib/rifRelayService/types.ts
@@ -0,0 +1,11 @@
+export interface RifRelayConfiguration {
+  preferedRelays: string[],
+  relayHubAddress: string,
+  relayVerifierAddress: string,
+  deployVerifierAddress: string,
+  smartWalletAddress: string,
+  relayWorkerAddress: string,
+  smartWalletFactoryAddress: string,
+  testTokenAddress: string,
+  rskHost: string,
+}
