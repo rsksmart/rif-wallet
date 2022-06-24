bc4ad25f9c23d4d33fad1f4680379c28b20c5481
Francis Rodriguez <francisngg@gmail.com>
2022-06-24 11:35:52 -0400

    Added language settings

--- src/screens/changeLanguage/ChangeLanguageScreen.tsx
+++ src/screens/changeLanguage/ChangeLanguageScreen.tsx
@@ -1,19 +1,44 @@
 import React from 'react'
-import { View } from 'react-native'
-import { Button } from '../../components'
-import { useTranslation, Trans } from 'react-i18next'
-import { Paragraph } from '../../components'
+import { View, StyleSheet } from 'react-native'
+import { RegularText } from '../../components'
+import { colors } from '../../styles'
+import ActiveButton from '../../components/button/ActiveButton'
 
 export const ChangeLanguageScreen = () => {
-  const { i18n } = useTranslation()
-
+  // @TODO logic to implement language change and mode change...
   return (
-    <View>
-      <Paragraph>
-        <Trans>Please select your language</Trans>
-      </Paragraph>
-      <Button onPress={() => i18n.changeLanguage('es')} title={'ES'} />
-      <Button onPress={() => i18n.changeLanguage('en')} title={'EN'} />
+    <View style={styles.container}>
+      <View>
+        <RegularText>Select language</RegularText>
+        <View style={styles.languageView}>
+          <ActiveButton text="ENG" />
+          <ActiveButton text="ESP" />
+        </View>
+      </View>
+      <View style={styles.modeView}>
+        <RegularText>Mode</RegularText>
+        <View style={styles.languageView}>
+          <ActiveButton text="dark mode" />
+          <ActiveButton text="light mode" />
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background.light,
+    paddingTop: 10,
+    paddingHorizontal: 40,
+  },
+  languageView: {
+    flexDirection: 'row',
+    paddingTop: 10,
+    justifyContent: 'center',
+  },
+  modeView: {
+    marginTop: 25,
+  },
+})
