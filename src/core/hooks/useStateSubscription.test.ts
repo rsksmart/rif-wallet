63de8f0eb1d5aba343fec077a13fbc83d19c0342
rodrigo.santana <rodrigo.santana@iovlabs.org>
2022-07-28 16:08:18 -0300

    fix useStateSubscription tests

--- src/core/hooks/useStateSubscription.test.ts
+++ src/core/hooks/useStateSubscription.test.ts
@@ -4,38 +4,66 @@ import { act } from 'react-test-renderer'
 import { useStateSubscription } from './useStateSubscription'
 
 describe('hook: useStateSubscription', () => {
-  let result: any
-  let appStateSpy: jest.SpyInstance
-  beforeEach(() => {
-    ;({ result } = renderHook(() => useStateSubscription()))
-    appStateSpy = jest.spyOn(AppState, 'addEventListener')
-  })
+  // in order to test the hook, we should test all the different scenarios in a single test
+  test('test some different scenarios', () => {
+    const { result } = renderHook(() => useStateSubscription())
+    const appStateSpy = jest.spyOn(AppState, 'addEventListener')
 
-  test('renders the hook with default values', () => {
+    // 1. initial state
     act(() => {
       appStateSpy.mock.calls[0][1]('active')
     })
 
     expect(result.current.unlocked).toBeFalsy()
     expect(result.current.active).toBeTruthy()
-  })
 
-  test('should change unlocked to true', () => {
+    // 2. unlock the app
     act(() => {
-      appStateSpy.mock.calls[0][1]('active')
       result.current.setUnlocked(true)
     })
 
     expect(result.current.unlocked).toBeTruthy()
     expect(result.current.active).toBeTruthy()
-  })
 
-  test('should change active to false after app goes to background', () => {
+    // 3. put the app to background
     act(() => {
       appStateSpy.mock.calls[0][1]('background')
     })
 
+    expect(result.current.unlocked).toBeTruthy()
+    expect(result.current.active).toBeFalsy()
+
+    // 4. background -> active
+    act(() => {
+      appStateSpy.mock.calls[0][1]('active')
+    })
+
+    expect(result.current.unlocked).toBeTruthy()
+    expect(result.current.active).toBeTruthy()
+
+    // 5. put the app to background again and lock it
+    act(() => {
+      appStateSpy.mock.calls[0][1]('background')
+      result.current.setUnlocked(false)
+    })
+
     expect(result.current.unlocked).toBeFalsy()
     expect(result.current.active).toBeFalsy()
+
+    // 6. background -> active
+    act(() => {
+      appStateSpy.mock.calls[0][1]('active')
+    })
+
+    expect(result.current.unlocked).toBeFalsy()
+    expect(result.current.active).toBeTruthy()
+
+    // 7. unlock the app
+    act(() => {
+      result.current.setUnlocked(true)
+    })
+
+    expect(result.current.unlocked).toBeTruthy()
+    expect(result.current.active).toBeTruthy()
   })
 })
