76f011cba3e1b8fddd227af46796278d565d2652
Jesse Clark <hello@developerjesse.com>
2022-09-12 14:57:25 +0300

    Revert modal.

--- src/screens/send/WalletNotDeployedModal.tsx
+++ src/screens/send/WalletNotDeployedModal.tsx
@@ -1,72 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Image, StyleSheet, View } from 'react-native'
 import { RegularText, SemiBoldText } from '../../components'
 import { colors } from '../../styles'
 import PrimaryButton from '../../components/button/PrimaryButton'
 import { Arrow } from '../../components/icons'
-import { RIFWallet } from '../../lib/core'
-import { RelayPayment } from '../../lib/relay-sdk/types'
 
-interface Interface {
-  wallet: RIFWallet
+type WalletNotDeployedViewType = {
+  onDeployWalletPress: () => void
 }
-
-const WalletNotDeployedView: React.FC<Interface> = ({ wallet }) => {
-  const [isDeploying, setIsDeploying] = useState<boolean>(false)
-
-  const payment: RelayPayment = {
-    tokenContract: '0x19f64674d8a5b4e652319f5e239efd3bc969a1fe',
-    tokenAmount: '0',
-  }
-
-  const onDeployWalletPress = () => {
-    setIsDeploying(true)
-    wallet
-      .deploySmartWallet(payment)
-      .then((response: any) => {
-        console.log('is deployed!', response)
-      })
-      .finally(() => setIsDeploying(false))
-  }
-
-  if (isDeploying) {
-    return (
-      <View style={styles.container} testID="WalletNotDeployedView">
-        <SemiBoldText style={styles.oopsText}>
-          Deploying your smartwallet now!
-        </SemiBoldText>
-      </View>
-    )
-  }
-
-  return (
-    <View style={styles.container} testID="WalletNotDeployedView">
-      <Image
-        source={require('../../images/undeployed_wallet.png')}
-        style={styles.imageStyle}
-        resizeMode="contain"
-      />
-      <View>
-        <SemiBoldText style={styles.oopsText}>
-          Oops... Your wallet has not yet been deployed.
-        </SemiBoldText>
-        <RegularText style={styles.regularText}>
-          To be able to send funds, you need to deploy your wallet first.
-        </RegularText>
-        <PrimaryButton
-          style={styles.buttonDeploy}
-          onPress={onDeployWalletPress}>
-          <View style={styles.buttonView}>
-            <Arrow color={'white'} rotate={45} width={35} height={35} />
-            <View>
-              <RegularText style={styles.deployText}>deploy wallet</RegularText>
-            </View>
+const WalletNotDeployedView: React.FC<WalletNotDeployedViewType> = ({
+  onDeployWalletPress,
+}) => (
+  <View style={styles.container} testID="WalletNotDeployedView">
+    <Image
+      source={require('../../images/undeployed_wallet.png')}
+      style={styles.imageStyle}
+      resizeMode="contain"
+    />
+    <View>
+      <SemiBoldText style={styles.oopsText}>
+        Oops... Your wallet has not yet been deployed.
+      </SemiBoldText>
+      <RegularText style={styles.regularText}>
+        To be able to send funds, you need to deploy your wallet first.
+      </RegularText>
+      <PrimaryButton style={styles.buttonDeploy} onPress={onDeployWalletPress}>
+        <View style={styles.buttonView}>
+          <Arrow color={'white'} rotate={45} width={35} height={35} />
+          <View>
+            <RegularText style={styles.deployText}>deploy wallet</RegularText>
           </View>
-        </PrimaryButton>
-      </View>
+        </View>
+      </PrimaryButton>
     </View>
-  )
-}
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
