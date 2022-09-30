5e4818fd14a8188d415ccd2700ca53fea5935e7b
Jesse Clark <hello@developerjesse.com>
2022-09-30 18:30:34 +0300

    Create reusable textInput and use it on the feedback screen.

---
+++ src/components/input/TextInputWithLabel.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { StyleSheet, TextInput, View } from 'react-native'
+import { colors } from '../../styles'
+import { fonts } from '../../styles/fonts'
+import { RegularText } from '../typography'
+
+interface TextInputInterface {
+  label: string
+  placeholder?: string
+  testID?: string
+  value: string
+  setValue: (value: string) => void
+  multiline?: boolean
+  inputStyle?: any
+}
+
+export const TextInputWithLabel: React.FC<TextInputInterface> = ({
+  label,
+  placeholder,
+  testID,
+  value,
+  setValue,
+  multiline,
+  inputStyle,
+}) => {
+  const inputStyles = inputStyle
+    ? { ...inputStyle, ...styles.input }
+    : styles.input
+  return (
+    <View>
+      <RegularText style={styles.label}>{label}</RegularText>
+      <TextInput
+        testID={testID}
+        accessibilityLabel="nameInput"
+        style={inputStyles}
+        onChangeText={setValue}
+        value={value}
+        placeholder={placeholder}
+        placeholderTextColor={colors.text.secondary}
+        multiline={multiline || false}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  label: {
+    color: colors.white,
+    paddingLeft: 5,
+    paddingBottom: 10,
+  },
+  input: {
+    color: colors.text.primary,
+    fontFamily: fonts.regular,
+    backgroundColor: colors.darkPurple4,
+    borderRadius: 15,
+    padding: 20,
+    marginBottom: 20,
+  },
+})
--- src/screens/feedback/FeedbackScreen.tsx
+++ src/screens/feedback/FeedbackScreen.tsx
@@ -1,12 +1,72 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { RegularText } from '../../components'
+import { PurpleButton } from '../../components/button/ButtonVariations'
+import { TextInputWithLabel } from '../../components/input/TextInputWithLabel'
+import { colors } from '../../styles'
 
 interface FeedbackInterface {}
 
 export const FeedbackScreen: React.FC<FeedbackInterface> = ({}) => {
+  const [isSent, setIsSent] = useState<boolean>(false)
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [feedback, setFeedback] = useState<string>('')
+
+  const submitFeedback = () => {
+    setIsSent(true)
+  }
+
+  if (isSent) {
+    return <RegularText>Thank you!</RegularText>
+  }
+
   return (
-    <View>
-      <Text>Feedback Form</Text>
+    <View style={styles.container}>
+      <RegularText style={styles.heading}>Feedback form</RegularText>
+
+      <TextInputWithLabel
+        label="Name"
+        placeholder="your name"
+        value={name}
+        setValue={setName}
+      />
+
+      <TextInputWithLabel
+        label="Email"
+        placeholder="your email"
+        value={email}
+        setValue={setEmail}
+      />
+
+      <TextInputWithLabel
+        label="Comments"
+        placeholder="your feedback"
+        value={feedback}
+        setValue={setFeedback}
+        multiline={true}
+        inputStyle={styles.feedback}
+      />
+
+      <PurpleButton title="Submit" onPress={submitFeedback} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.darkPurple3,
+    paddingHorizontal: 20,
+    height: '100%',
+  },
+  heading: {
+    color: colors.white,
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 20,
+  },
+  feedback: {
+    paddingTop: 20,
+    height: 150,
+  },
+})
