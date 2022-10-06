8d156893068417530f846f4882032bcebaa28def
Jesse Clark <hello@developerjesse.com>
2022-10-06 16:24:57 +0300

    STASH

--- src/screens/send/SendScreen.tsx
+++ src/screens/send/SendScreen.tsx
@@ -1,104 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, ScrollView, Text } from 'react-native'
-import { BigNumber, utils, ContractTransaction } from 'ethers'
-import { useSocketsState } from '../../subscriptions/RIFSockets'
-import {
-  convertToERC20Token,
-  makeRBTCToken,
-} from '../../lib/token/tokenMetadata'
-import { ScreenProps } from '../../RootNavigation'
-import { ScreenWithWallet } from '../types'
+
 import TransactionInfo, { transactionInfo } from './TransactionInfo'
 import { colors } from '../../styles'
 import TransactionForm from './TransactionForm'
 import { ITokenWithBalance } from '../../lib/rifWalletServices/RIFWalletServicesTypes'
 import WalletNotDeployedView from './WalletNotDeployedModal'
 
-export const SendScreen: React.FC<ScreenProps<'Send'> & ScreenWithWallet> = ({
-  route,
-  wallet,
+interface Interface {
+  isWalletDeployed: boolean
+  navigateToDeploySceen: () => void
+  handleTransfer: (token: any, amount: string, to: string) => Promise<string>
+  balances: ITokenWithBalance[]
+  prices: any
+  transactions: any
+  chainId: number
+}
+
+export const SendScreen: React.FC<Interface> = ({
   isWalletDeployed,
-  navigation,
+  navigateToDeploySceen,
+  handleTransfer,
+  balances,
+  prices,
+  transactions,
+  chainId,
 }) => {
-  const { state } = useSocketsState()
-  const contractAddress =
-    route.params?.contractAddress || Object.keys(state.balances)[0]
   const [currentTransaction, setCurrentTransaction] =
     useState<transactionInfo | null>(null)
   const [error, setError] = useState<Error>()
 
-  const [chainId, setChainId] = useState<number>(31)
-
-  useEffect(() => {
-    wallet.getChainId().then(setChainId)
-  }, [wallet])
-
-  const transfer = (token: ITokenWithBalance, amount: string, to: string) => {
-    setError(undefined)
-    setCurrentTransaction({ status: 'USER_CONFIRM' })
-
-    // handle both ERC20 tokens and the native token (gas)
-    const transferMethod =
-      token.symbol === 'TRBTC'
-        ? makeRBTCToken(wallet, chainId)
-        : convertToERC20Token(token, {
-            signer: wallet,
-            chainId,
-          })
-
-    transferMethod.decimals().then((decimals: number) => {
-      const tokenAmount = BigNumber.from(utils.parseUnits(amount, decimals))
-
-      transferMethod
-        .transfer(to.toLowerCase(), tokenAmount)
-        .then((txPending: ContractTransaction) => {
-          const current: transactionInfo = {
-            to,
-            value: amount,
-            symbol: transferMethod.symbol,
-            hash: txPending.hash,
-            status: 'PENDING',
-          }
-          setCurrentTransaction(current)
-
-          txPending
-            .wait()
-            .then(() =>
-              setCurrentTransaction({ ...current, status: 'SUCCESS' }),
-            )
-            .catch(() =>
-              setCurrentTransaction({ ...current, status: 'FAILED' }),
-            )
-        })
-        .catch((err: any) => {
-          setError(err)
-          setCurrentTransaction(null)
-        })
-    })
-  }
-
-  const onDeployWalletNavigate = () =>
-    navigation.navigate('ManuallyDeployScreen' as any)
-
   return (
     <ScrollView style={styles.parent}>
       {!isWalletDeployed && (
-        <WalletNotDeployedView onDeployWalletPress={onDeployWalletNavigate} />
+        <WalletNotDeployedView onDeployWalletPress={navigateToDeploySceen} />
       )}
       {!currentTransaction ? (
         <TransactionForm
-          onConfirm={transfer}
-          tokenList={Object.values(state.balances)}
-          tokenPrices={state.prices}
+          onConfirm={handleTransfer}
+          tokenList={Object.values(balances)}
+          tokenPrices={prices}
           chainId={chainId}
           initialValues={{
-            recipient: route.params?.to,
+            recipient: '', // @jesse todo
             amount: '0',
-            asset: route.params?.contractAddress
-              ? state.balances[route.params.contractAddress]
-              : state.balances[contractAddress],
+            asset: balances[0], // @jesse todo
           }}
-          transactions={state.transactions.activityTransactions}
+          transactions={transactions.activityTransactions}
         />
       ) : (
         <TransactionInfo transaction={currentTransaction} />
--- src/screens/send/SendScreenContainer.tsx
+++ src/screens/send/SendScreenContainer.tsx
@@ -1,17 +1,100 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { RIFWallet } from '../../lib/core'
 import { NavigationProp } from '../../RootNavigation'
+import { useSocketsState } from '../../subscriptions/RIFSockets'
+import { ScreenWithWallet } from '../types'
+import { SendScreen } from './SendScreen'
 
 interface Interface {
   navigation: NavigationProp
 }
 
-export const SendScreenContainer: React.FC<Interface> = ({ navigation }) => {
-  // const navigateToDeployScreen = navigation.navigate.('depl')
+export const SendScreenContainer: React.FC<ScreenWithWallet & Interface> = ({
+  wallet,
+  isWalletDeployed,
+  navigation,
+}) => {
+  const handleTransfer = () => {
+    console.log('handling transfer...')
+    return Promise.reject('transaction cancelled')
+  }
+
+  const chainId = 31
+
+  const { state } = useSocketsState()
+  /*
+  const contractAddress =
+    route.params?.contractAddress || Object.keys(state.balances)[0]
+  const [currentTransaction, setCurrentTransaction] =
+    useState<transactionInfo | null>(null)
+  const [error, setError] = useState<Error>()
+
+  const [chainId, setChainId] = useState<number>(31)
+
+  useEffect(() => {
+    wallet.getChainId().then(setChainId)
+  }, [wallet])
+
+  const transfer = (token: ITokenWithBalance, amount: string, to: string) => {
+    setError(undefined)
+    setCurrentTransaction({ status: 'USER_CONFIRM' })
+
+    // handle both ERC20 tokens and the native token (gas)
+    const transferMethod =
+      token.symbol === 'TRBTC'
+        ? makeRBTCToken(wallet, chainId)
+        : convertToERC20Token(token, {
+            signer: wallet,
+            chainId,
+          })
+
+    transferMethod.decimals().then((decimals: number) => {
+      const tokenAmount = BigNumber.from(utils.parseUnits(amount, decimals))
+
+      transferMethod
+        .transfer(to.toLowerCase(), tokenAmount)
+        .then((txPending: ContractTransaction) => {
+          const current: transactionInfo = {
+            to,
+            value: amount,
+            symbol: transferMethod.symbol,
+            hash: txPending.hash,
+            status: 'PENDING',
+          }
+          setCurrentTransaction(current)
+
+          txPending
+            .wait()
+            .then(() =>
+              setCurrentTransaction({ ...current, status: 'SUCCESS' }),
+            )
+            .catch(() =>
+              setCurrentTransaction({ ...current, status: 'FAILED' }),
+            )
+        })
+        .catch((err: any) => {
+          setError(err)
+          setCurrentTransaction(null)
+        })
+    })
+  }
+
+  
+  */
+
+  const onDeployWalletNavigate = () =>
+    navigation.navigate('ManuallyDeployScreen' as any)
 
   return (
-    <View>
-      <Text>SendScreenContainer Component</Text>
-    </View>
+    <SendScreen
+      navigateToDeploySceen={onDeployWalletNavigate}
+      isWalletDeployed={isWalletDeployed}
+      handleTransfer={handleTransfer}
+      balances={state.balances}
+      prices={state.prices}
+      transactions={state.transactions}
+      chainId={chainId}
+    />
   )
 }
