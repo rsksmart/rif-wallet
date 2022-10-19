21c4dcc59f42e39cf732e506e75118cba1932129
rodrigo.santana <rodrigo.santana@iovlabs.org>
2022-10-19 01:00:04 -0300

    allow to edit account name

--- src/components/icons/EditMaterialIcon.tsx
+++ src/components/icons/EditMaterialIcon.tsx
@@ -1,9 +1,7 @@
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import React from 'react'
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import { MaterialIconInterface } from '.'
 
-const EditMaterialIcon = (props?: MaterialIconInterface) => (
+export const EditMaterialIcon = (props?: MaterialIconInterface) => (
   <MaterialIcon name="edit" {...props} />
 )
-
-export default EditMaterialIcon
--- src/screens/home/SelectedTokenComponent.tsx
+++ src/screens/home/SelectedTokenComponent.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
 import { BigNumberish } from 'ethers'
-import { RegularText } from '../../components'
+import React, { useState } from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import { RegularText } from '../../components'
 
+import { EditMaterialIcon, HideShowIcon } from '../../components/icons'
+import { CheckIcon } from '../../components/icons/CheckIcon'
 import { colors } from '../../styles'
-import { HideShowIcon } from '../../components/icons'
+import { fonts } from '../../styles/fonts'
 
 interface Interface {
   accountNumber?: number
@@ -18,17 +20,68 @@ const SelectedTokenComponent: React.FC<Interface> = ({
   amount,
   change,
 }) => {
+  const initialAccountName =
+    typeof accountNumber === 'number' ? `account ${accountNumber + 1}` : ''
+  const [accountName, setAccountName] = useState<string>(initialAccountName)
   const [showBalances, setShowBalances] = useState<boolean>(true)
+  const [showAccountInput, setShowAccountInput] = useState<boolean>(false)
   const badgeColor = change && change >= 0 ? styles.greenBadge : styles.redBadge
   const onSetBalances = () => setShowBalances(!showBalances)
+
+  const onChangeAccountName = (text: string) => {
+    if (text.length <= 30) {
+      setAccountName(text)
+    }
+  }
+
+  const onEdit = () => {
+    setShowAccountInput(true)
+  }
+
+  const onSubmit = () => {
+    if (accountName.length > 0) {
+      setAccountName(accountName.trim())
+      setShowAccountInput(false)
+    }
+  }
+
   return (
     <View style={styles.balanceCard}>
       <View style={styles.topRow}>
         {typeof accountNumber === 'number' && (
           <View style={styles.accountLabel}>
-            <RegularText style={styles.accountText}>{`account ${
-              accountNumber + 1
-            }`}</RegularText>
+            {!showAccountInput ? (
+              <>
+                <RegularText style={styles.accountText}>
+                  {accountName}
+                </RegularText>
+                <TouchableOpacity onPress={onEdit}>
+                  <EditMaterialIcon
+                    color={colors.darkPurple2}
+                    size={16}
+                    style={styles.editIcon}
+                  />
+                </TouchableOpacity>
+              </>
+            ) : (
+              <>
+                <TextInput
+                  autoFocus={true}
+                  style={styles.accountInput}
+                  value={accountName}
+                  onChangeText={onChangeAccountName}
+                />
+                <TouchableOpacity onPress={onSubmit}>
+                  <View>
+                    <CheckIcon
+                      color={colors.darkPurple2}
+                      width={30}
+                      height={30}
+                    />
+                  </View>
+                </TouchableOpacity>
+              </>
+            )}
           </View>
         )}
         <TouchableOpacity onPress={onSetBalances}>
@@ -80,10 +133,31 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   accountLabel: {
+    flexDirection: 'row',
     paddingTop: 5,
+    alignItems: 'center',
   },
   accountText: {
     color: colors.darkGray,
+    borderWidth: 1,
+    borderColor: colors.lightPurple,
+    paddingHorizontal: 5,
+    paddingTop: 3,
+    paddingBottom: 1,
+  },
+  accountInput: {
+    color: colors.darkGray,
+    fontFamily: fonts.regular,
+    borderWidth: 1,
+    borderColor: colors.darkPurple,
+    borderRadius: 5,
+    paddingHorizontal: 5,
+    paddingTop: 0,
+    paddingBottom: 0,
+  },
+  editIcon: {
+    marginLeft: 2,
+    marginBottom: 5,
   },
   badge: {
     backgroundColor: colors.lightGray,
