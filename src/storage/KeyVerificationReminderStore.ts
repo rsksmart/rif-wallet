93f9643030fa49d2c1eb316ae16e0fc515ba37bc
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-10-20 22:35:41 -0600

    Defer confirmation of the mnemonic until later

--- src/storage/KeyVerificationReminderStore.ts
+++ src/storage/KeyVerificationReminderStore.ts
@@ -6,9 +6,7 @@ const KeyVerificationReminderStore = createStore(key)
 export const hasKeyVerificationReminder = KeyVerificationReminderStore.has
 export const getKeyVerificationReminder = async () => {
   const result = await KeyVerificationReminderStore.get()
-  console.log({ result })
   return result === 'true'
 }
-export const deleteKeyVerificationReminder = KeyVerificationReminderStore.remove
 export const saveKeyVerificationReminder = (value: boolean) =>
   KeyVerificationReminderStore.save(value.toString())
