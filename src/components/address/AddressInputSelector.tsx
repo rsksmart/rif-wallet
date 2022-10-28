090c0cfd43199b53b89a51629f870c7b4ab7be05
Francis Rodriguez <francisngg@gmail.com>
2022-10-28 18:50:02 -0400

    Cleaned up AddressInputSelector.tsx
    Added documentation.
    Added interface.

--- src/components/address/AddressInputSelector.tsx
+++ src/components/address/AddressInputSelector.tsx
@@ -2,8 +2,28 @@ import { colors } from '../../styles'
 import { AddressInput } from './AddressInput'
 import React from 'react'
 import { AddressBitcoinInput } from './AddressBitcoinInput'
+import { MixedTokenAndNetworkType } from '../../screens/send/types'
 
-export const AddressInputSelector: React.FC<{ [key: string]: any }> = ({
+interface IAddressInputSelector {
+  token: MixedTokenAndNetworkType
+  initialValue: string
+  onChangeText: (newValue: string, isValid: boolean) => void
+  testID?: string
+  chainId: number
+  backgroundColor?: string
+}
+
+/**
+ * Returns either AddressInput for Bitcoin or for RSK Flow based on a prop "isBitcoin" in the token
+ * @param token
+ * @param initialValue
+ * @param onChangeText
+ * @param testID
+ * @param chainId
+ * @param backgroundColor
+ * @constructor
+ */
+export const AddressInputSelector: React.FC<IAddressInputSelector> = ({
   token = {},
   initialValue,
   onChangeText,
