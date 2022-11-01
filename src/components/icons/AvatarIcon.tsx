26a87d862aaf1cceb57fbaca2aa8ff2d2610aeb4
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2022-11-01 09:43:48 -0300

    US-1059: Avatar creation (#316)
    * add avatar generator lib
    
    * add avatar icon to search domain screen
    
    * change white to light purple on choose alias screen
    
    * add avatar icon to request domain screen
    
    * add .rsk suffix automatically
    
    * add avatar icon for futher steps
    
    * add alias icon to header
    
    * change alias icon background to white
    
    * For better readability
    
    * check if profile.alias exists
    
    * show placeholder image when no alias
    
    * remove no valid message for alias length less than 5 chars
    
    * styles
    
    * does not allow small alias
    
    * swap minus and plus icons
    
    * remove duplicate dependency

---
+++ src/components/icons/AvatarIcon.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Jdenticon from 'react-native-jdenticon'
+import { colors } from '../../styles'
+
+interface AvatarIconProps {
+  value: string
+  size?: number
+  style?: any
+}
+
+export const AvatarIcon: React.FC<AvatarIconProps> = ({
+  value,
+  size = 40,
+  style,
+}) => {
+  const config = {
+    lightness: {
+      color: [0.4, 0.8],
+      grayscale: [0.3, 0.6],
+    },
+    saturation: {
+      color: 0.7,
+      grayscale: 0.5,
+    },
+    padding: 0.15,
+  }
+  const iconStyle = {
+    borderRadius: 50,
+    backgroundColor: colors.white,
+    ...style,
+  }
+  return (
+    <Jdenticon value={value} size={size} style={iconStyle} config={config} />
+  )
+}
