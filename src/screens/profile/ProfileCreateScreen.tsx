04eeb51139c100e40cb58f345faa9bd67947cf6f
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-11-06 23:08:56 -0600

    feat: run alias registration in the background

--- src/screens/profile/ProfileCreateScreen.tsx
+++ src/screens/profile/ProfileCreateScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { IProfileStore } from '../../storage/ProfileStore'
 import { RegularText } from '../../components/typography'
 import { TextInput } from 'react-native-gesture-handler'
@@ -11,11 +11,6 @@ import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import { ScreenProps } from '../../RootNavigation'
 import { emptyProfile } from '../../core/hooks/useProfile'
 import { fonts } from '../../styles/fonts'
-import {
-  getAliasRegistration,
-  hasAliasRegistration,
-  IProfileRegistrationStore,
-} from '../../storage/AliasRegistrationStore'
 
 export type CreateProfileScreenProps = {
   route: any
@@ -38,31 +33,6 @@ export const ProfileCreateScreen: React.FC<
     await eraseProfile()
     navigation.navigate('Home')
   }
-  const checkRegistrationStatus = async () => {
-    let aliasRegistration: IProfileRegistrationStore
-    let alias: string
-    let duration: number
-    let hash: string
-    const hasStartedRegistration = await hasAliasRegistration()
-
-    if (hasStartedRegistration) {
-      aliasRegistration = await getAliasRegistration()
-      alias = aliasRegistration.alias
-      duration = aliasRegistration.duration
-      hash = aliasRegistration.commitToRegisterHash
-      if (hash) {
-        navigation.navigate('RequestDomain', {
-          navigation,
-          alias: alias,
-          duration: duration,
-        })
-      }
-    }
-  }
-
-  useEffect(() => {
-    checkRegistrationStatus().then()
-  }, [])
   return (
     <>
       <View style={styles.profileHeader}>
@@ -74,9 +44,11 @@ export const ProfileCreateScreen: React.FC<
         <MediumText style={styles.titleText}>
           {editProfile ? 'edit profile' : 'create profile'}
         </MediumText>
-        <TouchableOpacity onPress={() => deleteAlias()}>
-          <MaterialIcon name="delete" color="white" size={20} />
-        </TouchableOpacity>
+        {editProfile && (
+          <TouchableOpacity onPress={() => deleteAlias()}>
+            <MaterialIcon name="delete" color="white" size={20} />
+          </TouchableOpacity>
+        )}
       </View>
       <View style={styles.container}>
         <View style={styles.profileImageContainer}>
