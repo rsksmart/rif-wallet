2fe1026fad0c3d543da8c79ff32c72d483ed633b
Alexander Evchenko <alexanderevchenko@Alexanders-MacBook-Pro.local>
2022-11-09 14:22:05 +0400

    refactor: BitcoinNetworkStore remove sync, correct types

--- src/storage/BitcoinNetworkStore.ts
+++ src/storage/BitcoinNetworkStore.ts
@@ -3,39 +3,33 @@ import { Store } from './Store'
 const key = 'BITCOIN_NETWORK'
 const BitcoinStore = new Store(key)
 
-type BitcoinNetworkObjectType = {
-  name: string
-  bips: Array<string>
-}
-export type BitcoinNetworkType = {
-  [key: string]: BitcoinNetworkObjectType
+export interface BitcoinNetworks {
+  [key: string]: {
+    name: string
+    bips: Array<string>
+  }
 }
 
-const BitcoinNetworkStore = {
-  getStoredNetworks: async (): Promise<BitcoinNetworkType> => {
-    return (await BitcoinStore.get()) || {}
+export const BitcoinNetworkStore = {
+  getStoredNetworks: (): BitcoinNetworks => {
+    return BitcoinStore.get() || {}
   },
-  addNewNetwork: async (
-    networkName: string,
-    bips: Array<string> = [],
-  ): Promise<BitcoinNetworkObjectType> => {
-    const currentNetworks = await BitcoinNetworkStore.getStoredNetworks()
+  addNewNetwork: (networkName: string, bips: Array<string> = []) => {
+    const currentNetworks = BitcoinNetworkStore.getStoredNetworks()
     const network = {
       name: networkName,
       bips,
     }
 
     currentNetworks[networkName] = network
-    await BitcoinStore.set(currentNetworks)
+    BitcoinStore.set(currentNetworks)
     return network
   },
-  deleteNetwork: async (networkName: string): Promise<boolean> => {
-    const currentNetworks = await BitcoinNetworkStore.getStoredNetworks()
+  deleteNetwork: (networkName: string): boolean => {
+    const currentNetworks = BitcoinNetworkStore.getStoredNetworks()
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { [networkName]: removed, ...newNetworks } = currentNetworks
-    await BitcoinStore.set(newNetworks)
+    BitcoinStore.set(newNetworks)
     return true
   },
 }
-
-export default BitcoinNetworkStore
