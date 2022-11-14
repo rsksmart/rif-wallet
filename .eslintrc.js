dc0ddfef58c81337308728c5c499da03856b6fde
Alexander Evchenko <alexanderevchenko@Alexanders-MacBook-Pro.local>
2022-11-14 13:16:29 +0400

    chore: add @babel/plugin-transform-react-jsx, react-native-screens packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:react/jsx-runtime'],
   rules: {
     semi: 'off',
     'eslint-comments/no-unlimited-disable': 'off',
--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      { useTransformReactJSXExperimental: true },
+    ],
+  ],
   plugins: [
     [
       'react-native-reanimated/plugin',
       {
         globals: ['__scanCodes'],
+      },
+    ],
+    [
+      '@babel/plugin-transform-react-jsx',
+      {
+        runtime: 'automatic',
       },
     ],
   ],
