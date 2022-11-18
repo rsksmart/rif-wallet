021219144cfa2c747876cad7ef8a9bcbad7dd5a9
Jesse Clark <hello@developerjesse.com>
2022-11-18 16:57:27 +0200

    hotfix: remove export of test file.

--- src/screens/createKeys/new/index.ts
+++ src/screens/createKeys/new/index.ts
@@ -1,6 +1,5 @@
 export * from './ConfirmNewMasterKeyScreen'
 export * from './NewMasterKeyScreen'
 export * from './WordSelector'
-export * from './WordSelector.test'
 export * from './WordView'
 export * from './styles'
