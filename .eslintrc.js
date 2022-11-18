128d93d683b5fa5e37c5b8db78cecc487c4eb4d6
Alexander Evchenko <alexanderevchenko@Alexanders-MacBook-Pro.local>
2022-11-18 10:55:00 +0400

    chore: add linting and typescript rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,17 @@ module.exports = {
   extends: [
     '@react-native-community',
     'plugin:react/jsx-runtime',
-    // 'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  // plugins: [
-  //   'react',
-  //   '@typescript-eslint',
-  //   'eslint-plugin-react',
-  //   'eslint-plugin-import',
-  //   'react-hooks',
-  //   'react-native',
-  // ],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'eslint-plugin-react',
+    'eslint-plugin-import',
+    'react-hooks',
+    'react-native',
+  ],
   rules: {
     semi: 'off',
     'eslint-comments/no-unlimited-disable': 'off',
@@ -21,6 +21,7 @@ module.exports = {
     'no-undef': 'off',
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/no-empty-function': 'off',
   },
   settings: {
     'import/resolver': {
