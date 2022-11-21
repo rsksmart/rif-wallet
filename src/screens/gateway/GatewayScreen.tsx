3eb5ba91a2e346bd15374329e66021ea3542caf9
Jesse Clark <hello@developerjesse.com>
2022-11-21 19:04:16 +0200

    Add basic functionality and comments.

--- src/screens/gateway/GatewayScreen.tsx
+++ src/screens/gateway/GatewayScreen.tsx
@@ -1,21 +1,57 @@
-import { BigNumber } from 'ethers'
-import { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
-import { MediumText } from 'src/components'
+import { useEffect, useState } from 'react'
+import { BigNumber } from 'ethers'
 
-interface Props {}
+import { SemiBoldText, MediumText } from 'src/components'
+import { useSelectedWallet } from 'src/Context'
+import { AddressCopyComponent } from 'src/components/copy/AddressCopyComponent'
+
+// interface Props {}
 
 export const GatewayScreen = () => {
-  const [rifTokenBalance, setRifTokenBalance] = useState<BigNumber>(
-    BigNumber.from(0),
-  )
+  // RIFWallet (the smartwallet):
+  const { wallet, chainId } = useSelectedWallet()
+
+  // the RPC Provider:
+  const provider = wallet.provider
+
+  // the signer (i.e. the EOA account)
+  const signer = wallet.smartWallet.signer
+
+  const [rbtcBalance, setRbtcBalance] = useState<BigNumber>(BigNumber.from(0))
+  const [eoaAddress, setEoaAddress] = useState<string>('')
+  const [block, setBlock] = useState<number>(0)
+
+  // the following should all outside of this component, but it is for demostration right now
+  useEffect(() => {
+    signer.getAddress().then(setEoaAddress)
+  }, [wallet])
+
+  useEffect(() => {
+    signer.getBalance().then(setRbtcBalance)
+  }, [wallet])
+
+  useEffect(() => {
+    provider?.getBlockNumber().then(setBlock)
+  })
 
   return (
-    <View>
-      <MediumText>Gateway Screen</MediumText>
-      <MediumText>RIF Balance: {rifTokenBalance.toString()}</MediumText>
+    <View style={styles.parent}>
+      <SemiBoldText style={styles.heading}>Gateway Screen</SemiBoldText>
+      <MediumText>Chain Id: {chainId}</MediumText>
+      <MediumText>Block: {block}</MediumText>
+      <MediumText>Address:</MediumText>
+      <AddressCopyComponent address={eoaAddress} />
+      <MediumText>rBTC Balance: {rbtcBalance.toString()}</MediumText>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  parent: {
+    padding: 20,
+  },
+  heading: {
+    fontSize: 26,
+  },
+})
