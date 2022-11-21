9012ce288ae963766be1cb1e5821e380b55e3264
Alexander Ivchenko <47615361+TravellerOnTheRun@users.noreply.github.com>
2022-11-21 19:44:03 +0400

    US-1025 Review and possible fix of multiple SafeAreaView in Core.tsx (#352)
    * fix: two SafeAreas in Core.tsx
    
    * fix: put back the handleUpdatePin function
    
    Co-authored-by: Alexander Evchenko <alexanderevchenko@Alexanders-MacBook-Pro.local>

--- src/core/CoreGlobalErrorHandler.tsx
+++ src/core/CoreGlobalErrorHandler.tsx
@@ -1,11 +1,14 @@
 import { GlobalErrorHandler } from '../components/GlobalErrorHandler'
 import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary'
 import { CoreWithStore } from './CoreWithStore'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export const CoreGlobalErrorHandler = ({ CoreComponent = CoreWithStore }) => (
   <GlobalErrorHandler>
     <ErrorBoundary>
-      <CoreComponent />
+      <SafeAreaProvider>
+        <CoreComponent />
+      </SafeAreaProvider>
     </ErrorBoundary>
   </GlobalErrorHandler>
 )
