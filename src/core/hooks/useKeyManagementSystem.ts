f65f2f9f19a6bb500412abed8375b76ef748e31c
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-11-22 09:24:14 -0600

    fix: fix conflict

--- src/core/hooks/useKeyManagementSystem.ts
+++ src/core/hooks/useKeyManagementSystem.ts
@@ -9,7 +9,12 @@ import {
   deleteContacts,
   deleteKeys,
 } from '../../storage/MainStorage'
-import { addNextWallet, creteKMS, deleteCache, loadExistingWallets } from '../operations'
+import {
+  addNextWallet,
+  creteKMS,
+  deleteCache,
+  loadExistingWallets,
+} from '../operations'
 
 import { createRIFWalletFactory, networkId } from '../setup'
 
