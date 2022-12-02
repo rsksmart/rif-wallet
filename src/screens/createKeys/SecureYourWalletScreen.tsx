ae5948065038af718a48fe0a50e87c10aa36ca06
Jesse Clark <hello@developerjesse.com>
2022-12-02 17:07:55 +0200

    Remove character.

--- src/screens/createKeys/SecureYourWalletScreen.tsx
+++ src/screens/createKeys/SecureYourWalletScreen.tsx
@@ -27,7 +27,7 @@ export const SecureYourWalletScreen: React.FC<
     <View style={styles.parent}>
       <TouchableOpacity
         onPress={() => navigation.navigate('CreateKeys')}
-        accessibilityLabel="backß">
+        accessibilityLabel="back">
         <View style={styles.returnButtonView}>
           <Arrow color={colors.white} rotate={270} width={30} height={30} />
         </View>
