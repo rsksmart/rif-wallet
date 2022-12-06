fc2229244494c1f8290325e5be42e72fdea72b1e
Francis Rodriguez <francisngg@gmail.com>
2022-12-06 09:44:01 -0400

    Added transactionsReducer (missed it in the PR)

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit'
 import { appStateReducer } from './slices/appStateSlice/appStateSlice'
 import { balancesReducer } from './slices/balancesSlice/balancesSlice'
 import { profileReducer } from './slices/profileSlice/profileSlice'
+import { transactionsReducer } from './slices/transactionsSlice/transactionsSlice'
 import { usdPriceReducer } from './slices/usdPricesSlice/usdPricesSlice'
 
 export default combineReducers({
   usdPrices: usdPriceReducer,
   balances: balancesReducer,
   appState: appStateReducer,
+  transactions: transactionsReducer,
   profile: profileReducer,
 })
