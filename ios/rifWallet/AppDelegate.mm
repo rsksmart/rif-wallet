9c6f57f888dc03437fbd542148b38bd32a221aeb
Alexander Ivchenko <47615361+TravellerOnTheRun@users.noreply.github.com>
2022-12-15 13:55:54 +0400

    chore: rename the app from sWallet to rifWallet (#385)
    Co-authored-by: Alexander Evchenko <alexanderevchenko@Alexanders-MacBook-Pro.local>

--- ios/rifWallet/AppDelegate.mm
+++ ios/rifWallet/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"sWallet", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"rifWallet", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
--- ios/rifWalletTests/rifWalletTests.m
+++ ios/rifWalletTests/rifWalletTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface sWalletTests : XCTestCase
+@interface rifWalletTests : XCTestCase
 
 @end
 
-@implementation sWalletTests
+@implementation rifWalletTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
