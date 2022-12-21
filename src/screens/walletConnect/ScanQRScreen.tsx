4b0b1631c7e7f0fb177bc9968c7b543c56e400bc
Francis Rodriguez <francisngg@gmail.com>
2022-12-21 19:12:09 -0400

    feat: added ethereum and bitcoin when a QR is read

--- src/screens/walletConnect/ScanQRScreen.tsx
+++ src/screens/walletConnect/ScanQRScreen.tsx
@@ -1,13 +1,15 @@
 import { useContext, useState } from 'react'
 
-import { QRCodeScanner } from '../../components/QRCodeScanner'
-import { WalletConnectContext } from './WalletConnectContext'
+import { isBitcoinAddressValid } from 'lib/bitcoin/utils'
 import { useAppSelector } from 'src/redux/storeUtils'
 import { selectActiveWallet } from 'store/slices/settingsSlice'
 import {
   rootStackRouteNames,
   RootStackScreenProps,
 } from 'navigation/rootNavigator'
+
+import { QRCodeScanner } from 'components/QRCodeScanner'
+import { WalletConnectContext } from './WalletConnectContext'
 
 export const ScanQRScreen = ({
   navigation,
@@ -18,16 +20,26 @@ export const ScanQRScreen = ({
   const [isConnecting, setIsConnecting] = useState(false)
 
   const onCodeRead = (data: string) => {
-    if (!isConnecting && wallet) {
-      setIsConnecting(true)
-      createSession(wallet, data)
+    // Metamask QR
+    if (data.startsWith('ethereum:')) {
+      navigation.navigate(rootStackRouteNames.Send, {
+        to: data.split('ethereum:')[1],
+      })
+    } else if (isBitcoinAddressValid(data)) {
+      navigation.navigate(rootStackRouteNames.Send, {
+        to: data,
+      })
+    } else if (data.startsWith('wc:')) {
+      if (!isConnecting && wallet) {
+        setIsConnecting(true)
+        createSession(wallet, data)
+        // wait for session request
+        navigation.reset({
+          index: 0,
+          routes: [{ name: rootStackRouteNames.WalletConnect }],
+        })
+      }
     }
-
-    // wait for session request
-    navigation.reset({
-      index: 0,
-      routes: [{ name: rootStackRouteNames.WalletConnect }],
-    })
   }
 
   const onClose = () => {
