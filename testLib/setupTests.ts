f7ef2f6accb352fe49a922a3f82f292f8759b8a1
Jesse Clark <hello@developerjesse.com>
2023-01-03 12:48:06 +0200

    Mock event emitter in tests.

--- testLib/setupTests.ts
+++ testLib/setupTests.ts
@@ -1,3 +1,6 @@
 jest.mock('@rsksmart/rns-resolver.js')
 // include this line for mocking react-native-gesture-handler
 import 'react-native-gesture-handler/jestSetup'
+
+// mock NativeEventEmitter for the RAMP SDK
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
