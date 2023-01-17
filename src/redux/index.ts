e797c50deba77a2c884c9b4702422bc8403d8055
Luis Chavarría <luis.chavarria@iovlabs.org>
2023-01-16 20:09:00 -0600

    fix: add reset main storage

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -6,9 +6,11 @@ export { Dispatch } from 'redux'
 import { deleteContacts } from 'store/slices/contactsSlice'
 import { resetSocketState } from 'store/shared/actions/resetSocketState'
 import { resetKeysAndPin } from 'store/slices/settingsSlice'
+import { resetMainStorage } from 'storage/MainStorage'
 
 export const resetReduxStates = (dispatch: AppDispatch) => {
   dispatch(deleteContacts())
   dispatch(resetKeysAndPin())
   dispatch(resetSocketState())
+  resetMainStorage()
 }
