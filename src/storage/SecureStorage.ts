8ba9eaa0df43ec50c71978c22a7897a8cbdb93d8
Alexander Evchenko <alexanderevchenko@Alexanders-MacBook-Pro.local>
2023-01-26 12:22:32 +0400

    refactor: change accessControl value to BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE

--- src/storage/SecureStorage.ts
+++ src/storage/SecureStorage.ts
@@ -2,7 +2,6 @@ import {
   setGenericPassword,
   ACCESS_CONTROL,
   getGenericPassword,
-  getSupportedBiometryType,
   resetGenericPassword,
   AUTHENTICATION_TYPE,
   ACCESSIBLE,
@@ -12,14 +11,10 @@ const keyManagement = 'KEY_MANAGEMENT'
 
 export const getKeys = async () => {
   try {
-    const supportedType = await getSupportedBiometryType()
-
-    console.log('SUPPORTED TYPE', supportedType)
-
     const keys = await getGenericPassword({
       accessible: ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY,
       authenticationType: AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
-      accessControl: ACCESS_CONTROL.BIOMETRY_ANY_OR_DEVICE_PASSCODE,
+      accessControl: ACCESS_CONTROL.BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE,
     })
     if (!keys) {
       return null
@@ -35,7 +30,7 @@ export const saveKeys = async (keysValue: string) =>
   setGenericPassword(keyManagement, keysValue, {
     accessible: ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY,
     authenticationType: AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
-    accessControl: ACCESS_CONTROL.BIOMETRY_ANY_OR_DEVICE_PASSCODE,
+    accessControl: ACCESS_CONTROL.BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE,
   })
 
 export const deleteKeys = () => resetGenericPassword()
