8d8b86789711948ae0bfcea656cce0de9cc607bc
rodrigoncalves <rodrigosg2000@gmail.com>
2023-01-25 22:09:16 -0300

    SecureYourWalletScreen

--- src/components/button/PrimaryButton.tsx
+++ src/components/button/PrimaryButton.tsx
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: colors.lightPurple,
-    fontSize: 15,
+    fontSize: 14,
   },
   textDisabled: {
     color: colors.lightPurple,
--- src/components/button/SecondaryButton.tsx
+++ src/components/button/SecondaryButton.tsx
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: colors.lightPurple,
-    fontSize: 15,
+    fontSize: 14,
   },
   textDisabled: {
     color: colors.lightPurple,
