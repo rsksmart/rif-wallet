21fac59e82e15d264a0f9b56a10b66d2be0b9928
rodrigoncalves <rodrigosg2000@gmail.com>
2023-01-27 12:28:55 -0300

    add react-native-background-timer

--- src/core/hooks/useStateSubscription.ts
+++ src/core/hooks/useStateSubscription.ts
@@ -7,18 +7,20 @@ import {
   setUnlocked as setIsUnlocked,
 } from 'store/slices/settingsSlice'
 
+import { Platform } from 'react-native'
+import BackgroundTimer from 'react-native-background-timer'
 import { useAppDispatch, useAppSelector } from 'store/storeUtils'
 import { useAppState } from './useAppState'
 
 const gracePeriod = 3000
-let timer: NodeJS.Timeout
+let timer: NodeJS.Timer
 
 export const useStateSubscription = () => {
   const dispatch = useAppDispatch()
   const active = useAppSelector(selectAppIsActive)
   const unlocked = useAppSelector(selectIsUnlocked)
   const { appState } = useAppState()
-  const timerRef = useRef<NodeJS.Timeout>(timer)
+  const timerRef = useRef<NodeJS.Timer>(timer)
 
   const setUnlocked = useCallback(
     (isUnlocked: boolean) => {
@@ -32,14 +34,32 @@ export const useStateSubscription = () => {
     dispatch(setAppIsActive(isNowActive))
 
     if (unlocked) {
+      console.log('useStateSubscription', 'app is unlocked')
       if (!isNowActive) {
-        timerRef.current = setTimeout(() => {
-          setUnlocked(false)
-          dispatch(removeKeysFromState())
-        }, gracePeriod)
+        console.log('useStateSubscription', 'app is inactive, setting timer')
+        if (Platform.OS === 'android') {
+          timerRef.current = BackgroundTimer.setTimeout(() => {
+            console.log('useStateSubscription', 'timer expired, locking app')
+            setUnlocked(false)
+            dispatch(removeKeysFromState())
+          }, gracePeriod)
+        } else {
+          timerRef.current = setTimeout(() => {
+            console.log('useStateSubscription', 'timer expired, locking app')
+            setUnlocked(false)
+            dispatch(removeKeysFromState())
+          }, gracePeriod)
+        }
       } else if (timerRef.current) {
-        clearTimeout(timerRef.current)
+        console.log('useStateSubscription', 'app is active, clearing timer')
+        if (Platform.OS === 'android') {
+          BackgroundTimer.clearTimeout(timerRef.current)
+        } else {
+          clearTimeout(timerRef.current)
+        }
       }
+    } else {
+      console.log('useStateSubscription', 'app is locked')
     }
   }, [unlocked, appState, dispatch, setUnlocked])
 
