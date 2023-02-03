c1fbddc4a2cf8708ed60ecb08fc8ab0b0f32a85f
Alexander Ivchenko <47615361+TravellerOnTheRun@users.noreply.github.com>
2023-02-03 13:46:44 +0400

    feat: add new colors (#443)
    Co-authored-by: Alexander Evchenko <alexanderevchenko@Alexanders-MacBook-Pro.local>

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -2,3 +2,11 @@ export const testIDs = {
   wordInput: 'input.wordInput',
   indexLabel: 'view.indexLabel',
 }
+
+export const sharedColors = {
+  primary: '#4B5CF0',
+  secondary: '#121212',
+  success: '#79C600',
+  warning: '#FF9100',
+  danger: '#E94141',
+}
