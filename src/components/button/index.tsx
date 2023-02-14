ad55bdd87dabcc6fceb107a6db2a83d69dfafccb
Luis Chavarría <luis.chavarria@iovlabs.org>
2023-02-14 11:08:31 -0600

    fix: keep legacy buttons

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -156,3 +156,17 @@ const styles = StyleSheet.create({
     fontSize: defaultFontSize,
   },
 })
+
+// Legacy buttons to remove
+export { ActiveButton } from './ActiveButton'
+export { DialButton } from './DialButton'
+export { OutlineButton } from './OutlineButton'
+export { PrimaryButton } from './PrimaryButton'
+export { SecondaryButton } from './SecondaryButton'
+export { TransferButton } from './TransferButton'
+export { WhiteTransparentButton } from './WhiteButton'
+
+// Button-like things
+export { PaginationNavigator } from './PaginationNavigator'
+export { ToggleButtons } from './ToggleButtons'
+export { TokenButton } from './TokenButton'
