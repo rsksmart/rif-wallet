663f0dfd7b0579c091e530c333de03889bdacb65
Sleyter Sandoval <sleyter.sandoval@iovlabs.org>
2023-02-15 22:59:32 -0500

    Add migration

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,6 +1,8 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import { persistReducer } from 'redux-persist'
-import { reduxStorage } from 'src/storage/ReduxStorage'
+import { combineReducers, Reducer } from '@reduxjs/toolkit'
+import { persistReducer, createMigrate } from 'redux-persist'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+
+import { reduxStorage } from 'storage/ReduxStorage'
 import { accountsReducer } from './slices/accountsSlice'
 import { balancesReducer } from './slices/balancesSlice'
 import { contactsReducer } from 'store/slices/contactsSlice'
@@ -8,14 +10,31 @@ import { profileReducer } from './slices/profileSlice'
 import { settingsSliceReducer } from './slices/settingsSlice'
 import { transactionsReducer } from './slices/transactionsSlice'
 import { usdPriceReducer } from './slices/usdPricesSlice'
+import { ProfileStatus } from 'navigation/profileNavigator/types'
+
+const migrations = {
+  // eslint-disable-next-line
+  0: (state: any) => ({
+    ...state,
+    profile: {
+      alias: '',
+      phone: '',
+      emails: '',
+      status: ProfileStatus.NONE,
+    },
+  }),
+}
 
 const persistConfig = {
   key: 'root',
   storage: reduxStorage,
+  version: 0,
+  stateReconciler: autoMergeLevel2,
+  migrate: createMigrate(migrations),
   whitelist: ['profile', 'accounts', 'contacts', 'balances', 'usdPrices'],
 }
 
-const reducers = combineReducers({
+const reducers: Reducer = combineReducers({
   usdPrices: usdPriceReducer,
   balances: balancesReducer,
   transactions: transactionsReducer,
