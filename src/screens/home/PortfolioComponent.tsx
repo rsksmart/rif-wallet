598591e60a50e69626ca800d7ae4a8488b0ff3fa
Luis Chavarría <luis.chavarria@iovlabs.org>
2023-02-23 07:39:12 -0600

    fix: clean code

--- src/screens/home/PortfolioComponent.tsx
+++ src/screens/home/PortfolioComponent.tsx
@@ -80,7 +80,7 @@ const PortfolioComponent = ({
 
   return (
     <View>
-      {/*This View above is fix to keep the ScrollView height*/}
+      {/*This View above is a fix to keep the ScrollView height*/}
       <ScrollView horizontal={true}>
         <PortfolioCard
           onPress={() => setIsTotalCardSelected(true)}
