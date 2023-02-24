547ec9f9e37eba5bc1566858b28e7cd670dd2776
Alexander Ivchenko <47615361+TravellerOnTheRun@users.noreply.github.com>
2023-02-24 19:55:16 +0400

    US-1426 RIF Wallet - View your mnemonic Screen (#476)
    * refactor: AppTouchable width should accept all the possible width values
    
    * refactor: AppButton component for better styling
    
    * feat: add EyeIcon component
    
    * refactor: AppTouchable default align center
    
    * feat: MnemonicComponent
    
    * feat: add disabled style to AppButton
    
    * feat: MnemonicComponent ToggleMnemonic function to pass mnemonic state to parent
    
    * feat: implemeneted NewMasterKeyScreen design
    
    * feat: NewMasterKey screen
    
    ---------
    
    Co-authored-by: Alexander Evchenko <alexanderevchenko@Alexanders-MacBook-Pro.local>

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -104,6 +104,15 @@ const resources = {
       header_purchase: 'Purchase username',
       header_purchasing: 'Purchasing username...',
       header_error: 'Error Requesting username',
+      mnemonic_title: 'Security advice',
+      mnemonic_title_copy: 'Copied to clipboard!',
+      mnemonic_body: `Please make sure that you are located 
+      in a safe environment with no one being
+      able to see your phone screen before you decide to reveal your phrase. `,
+      mnemonic_body_copy:
+        'We strongly recommend you to write the phrase also on a piece of paper.',
+      new_master_key_title: 'Save your phrase',
+      new_master_key_button_title: 'Phrase secured, Continue',
     },
   },
   es: {
