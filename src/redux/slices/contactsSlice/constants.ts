9f07ba988d81ad24ba24dfcd5de689c465491392
Francis Rodriguez <francisngg@gmail.com>
2023-03-14 10:11:24 -0400

    US-1422 - Added default contacts logic. This logic will allow the app to have a contact that cannot be edited or deleted, but could be selected or shown in another screen such as transaction summary when purchasing a domain.

---
+++ src/redux/slices/contactsSlice/constants.ts
@@ -0,0 +1,10 @@
+import { Contact } from 'shared/types'
+
+export const defaultContacts: Record<string, Contact> = {
+  '0x90734bd6bf96250a7b262e2bc34284b0d47c1e8d': {
+    address: '0x90734bd6bf96250a7b262e2bc34284b0d47c1e8d',
+    name: 'RNS Manager',
+    displayAddress: '0x90734bd6bf96250a7b262e2bc34284b0d47c1e8d',
+    isEditable: false,
+  },
+}
