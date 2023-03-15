b7b0cdf34061bd4b58e9253bb7ceb9f3345a8250
Francis Rodriguez <francisngg@gmail.com>
2023-03-15 19:16:42 -0400

    Removed empty <>

--- src/screens/activity/ActivityScreen.tsx
+++ src/screens/activity/ActivityScreen.tsx
@@ -79,17 +79,15 @@ export const ActivityScreen = ({
           />
         }
         ListHeaderComponent={
-          <>
-            <View style={styles.transactionsViewStyle}>
-              <Typography type="h2">{t('home_screen_transactions')}</Typography>
-              {transactionsCombined.length === 0 &&
-                btcTransactionFetcher.apiStatus !== 'fetching' && (
-                  <Typography type="h4" style={styles.listEmptyTextStyle}>
-                    {t('activity_list_empty')}
-                  </Typography>
-                )}
-            </View>
-          </>
+          <View style={styles.transactionsViewStyle}>
+            <Typography type="h2">{t('home_screen_transactions')}</Typography>
+            {transactionsCombined.length === 0 &&
+              btcTransactionFetcher.apiStatus !== 'fetching' && (
+                <Typography type="h4" style={styles.listEmptyTextStyle}>
+                  {t('activity_list_empty')}
+                </Typography>
+              )}
+          </View>
         }
         ListEmptyComponent={
           <>
