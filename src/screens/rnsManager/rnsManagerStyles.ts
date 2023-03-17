faaf2e1ec06aa6ca3722e8aa7f3d8f86e29f79f5
rodrigoncalves <rodrigosg2000@gmail.com>
2023-03-17 11:23:32 -0300

    wrap in castStyle

--- src/screens/rnsManager/rnsManagerStyles.ts
+++ src/screens/rnsManager/rnsManagerStyles.ts
@@ -1,71 +1,72 @@
 import { StyleSheet } from 'react-native'
 
+import { castStyle } from 'shared/utils/index'
 import { sharedColors } from 'shared/constants'
 import { colors } from 'src/styles'
 
 export const rnsManagerStyles = StyleSheet.create({
-  scrollContainer: {
+  scrollContainer: castStyle.view({
     backgroundColor: colors.background.black,
-  },
-  container: {
+  }),
+  container: castStyle.view({
     flex: 1,
     backgroundColor: colors.background.black,
     padding: 20,
-  },
-  title: {
+  }),
+  title: castStyle.text({
     color: sharedColors.subTitle,
-  },
-  subtitle: {
+  }),
+  subtitle: castStyle.text({
     color: sharedColors.subTitle,
-  },
-  profileHeader: {
+  }),
+  profileHeader: castStyle.view({
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingVertical: 20,
     paddingHorizontal: 10,
     backgroundColor: sharedColors.secondary,
-  },
-  profileImageContainer: {
+  }),
+  profileImageContainer: castStyle.view({
     alignItems: 'center',
     backgroundColor: sharedColors.inputInactive,
     borderRadius: 10,
     paddingVertical: 20,
-  },
-  profileImage: {
+  }),
+  profileImage: castStyle.view({
     width: 80,
     height: 80,
     borderRadius: 100,
-  },
-  profileDisplayAlias: {
+  }),
+  profileDisplayAlias: castStyle.view({
     alignSelf: 'center',
     padding: 10,
     paddingBottom: 0,
-  },
-  aliasRequestInfo: {
+  }),
+  aliasRequestInfo: castStyle.text({
     color: colors.lightPurple,
     alignSelf: 'center',
     padding: 5,
     paddingTop: 10,
-  },
-  aliasRequestInfo2: {
+  }),
+  aliasRequestInfo2: castStyle.text({
     color: colors.gray,
     alignSelf: 'center',
     padding: 5,
-  },
-  marginTop: {
+  }),
+  marginTop: castStyle.view({
     marginTop: 10,
-  },
-  marginBottom: {
+  }),
+  marginBottom: castStyle.view({
     marginBottom: 10,
-  },
-  bottomContainer: {
+  }),
+  bottomContainer: castStyle.view({
     flex: 1,
     justifyContent: 'flex-end',
-  },
-  button: {
+  }),
+  button: castStyle.view({
     marginTop: 20,
-  },
-  disabledButton: {
+  }),
+  disabledButton: castStyle.view({
     opacity: 0.4,
-  },
+  }),
 })
