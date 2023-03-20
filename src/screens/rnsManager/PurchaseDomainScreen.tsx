f06437eda523e524bb4820d6232de654a100605e
rodrigoncalves <rodrigosg2000@gmail.com>
2023-03-20 14:16:31 -0300

    fix find rif token

--- src/screens/rnsManager/PurchaseDomainScreen.tsx
+++ src/screens/rnsManager/PurchaseDomainScreen.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { StyleSheet, View } from 'react-native'
