a037004141e93276c4bdf09a0ec87dbebf5b5a2e
rodrigoncalves <rodrigosg2000@gmail.com>
2023-04-04 12:45:21 -0300

    remove console log

--- src/screens/transactionSummary/index.tsx
+++ src/screens/transactionSummary/index.tsx
@@ -88,7 +88,6 @@ export const TransactionSummary = ({
   useFocusEffect(
     useCallback(() => {
       const onBackPress = () => {
-        console.log('back pressed')
         goBack()
         return true
       }
