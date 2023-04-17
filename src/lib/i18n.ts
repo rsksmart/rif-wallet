4a922f1357004ea69855f25b6ff4bbc785239969
Francis Rodriguez <francisngg@gmail.com>
2023-04-17 10:46:07 -0400

    Added text from translation

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -249,6 +249,10 @@ const resources = {
         'Do you want to disconnect from the Dapp',
       dapps_confirm_disconnection_confirm: 'Disconnect',
       dapps_confirm_disconnection_cancel: 'Reject',
+      alias_bought_congratulations: 'Congratulations!',
+      alias_bought_you_requested_domain: 'You have requested your domain.',
+      alias_bought_transaction_processing:
+        'Your transactions is being processed.',
     },
   },
   es: {
