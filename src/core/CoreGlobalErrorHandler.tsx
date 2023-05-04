0860e2be4a33c92c5e262ddb39c103cc02ee016c
Rodrigo Santana Gonçalves <rodrigosg2000@gmail.com>
2023-05-04 09:36:14 -0300

    hide the app when app is minimized (#570)

--- src/core/CoreGlobalErrorHandler.tsx
+++ src/core/CoreGlobalErrorHandler.tsx
@@ -1,14 +1,27 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Platform } from 'react-native'
+
 import { GlobalErrorHandler } from '../components/GlobalErrorHandler'
 import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary'
 import { CoreWithStore } from './CoreWithStore'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { useAppState } from './hooks/useAppState'
 
-export const CoreGlobalErrorHandler = ({ CoreComponent = CoreWithStore }) => (
-  <GlobalErrorHandler>
-    <ErrorBoundary>
-      <SafeAreaProvider>
-        <CoreComponent />
-      </SafeAreaProvider>
-    </ErrorBoundary>
-  </GlobalErrorHandler>
-)
+export const CoreGlobalErrorHandler = ({ CoreComponent = CoreWithStore }) => {
+  const { appState } = useAppState()
+
+  // Hide app when it's in background or inactive state
+  // For Android we must set FLAG_SECURE in MainActivity
+  // For iOS we must not render the CoreComponent (or build a custom screen)
+  const isNotActive = ['background', 'inactive'].includes(appState)
+  const hideApp = Platform.OS === 'ios' && isNotActive
+
+  return (
+    <GlobalErrorHandler>
+      <ErrorBoundary>
+        <SafeAreaProvider>
+          {hideApp ? null : <CoreComponent />}
+        </SafeAreaProvider>
+      </ErrorBoundary>
+    </GlobalErrorHandler>
+  )
+}
