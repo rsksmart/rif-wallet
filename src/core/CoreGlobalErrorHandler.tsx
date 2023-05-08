e2c68e25baf3e3c58bdb65d64f56e992ca1d065b
Alexander Evchenko <alexanderevchenko@Alexanders-MacBook-Pro.local>
2023-05-08 12:09:38 +0400

    fix: endless unlocking cycle

--- src/core/CoreGlobalErrorHandler.tsx
+++ src/core/CoreGlobalErrorHandler.tsx
@@ -1,25 +1,15 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { Platform } from 'react-native'
 
 import { GlobalErrorHandler } from '../components/GlobalErrorHandler'
 import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary'
 import { CoreWithStore } from './CoreWithStore'
-import { useAppState } from './hooks/useAppState'
 
 export const CoreGlobalErrorHandler = ({ CoreComponent = CoreWithStore }) => {
-  const { appState } = useAppState()
-
-  // Hide app when it's in background or inactive state
-  // For Android we must set FLAG_SECURE in MainActivity
-  // For iOS we must not render the CoreComponent (or build a custom screen)
-  const isNotActive = ['background', 'inactive'].includes(appState)
-  const hideApp = Platform.OS === 'ios' && isNotActive
-
   return (
     <GlobalErrorHandler>
       <ErrorBoundary>
         <SafeAreaProvider>
-          {hideApp ? null : <CoreComponent />}
+          <CoreComponent />
         </SafeAreaProvider>
       </ErrorBoundary>
     </GlobalErrorHandler>
