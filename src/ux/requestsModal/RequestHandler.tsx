cb8acada811857b5b2c94b7de8d70aaee918ab3c
Francis Rodriguez <39339295+Freshenext@users.noreply.github.com>
2023-05-19 07:18:45 -0400

    US-1651 Fixed bitcoin send flow (#595)

--- src/ux/requestsModal/RequestHandler.tsx
+++ src/ux/requestsModal/RequestHandler.tsx
@@ -4,21 +4,11 @@ import { useCallback } from 'react'
 //   SignMessageRequest,
 //   SignTypedDataRequest,
 // } from '@rsksmart/rif-wallet-core'
-import { useTranslation } from 'react-i18next'
 import { BigNumberish } from 'ethers'
-import { convertSatoshiToBtcHuman } from '@rsksmart/rif-wallet-bitcoin'
-
-import { convertTokenToUSD } from 'lib/utils'
 
 import { RequestWithBitcoin } from 'shared/types'
-import { navigationContainerRef } from 'core/Core'
-import { rootTabsRouteNames } from 'navigation/rootNavigator'
 // import { useFetchBitcoinNetworksAndTokens } from 'screens/send/useFetchBitcoinNetworksAndTokens'
-import { TokenSymbol } from 'screens/home/TokenImage'
-import { sharedColors } from 'shared/constants'
-import { AppButtonBackgroundVarietyEnum } from 'src/components'
-import { useAppSelector } from 'store/storeUtils'
-import { selectUsdPrices } from 'store/slices/usdPricesSlice'
+import { ReviewBitcoinTransactionContainer } from 'src/ux/requestsModal/ReviewBitcoinTransactionContainer'
 
 import { ReviewTransactionContainer } from './ReviewRelayTransaction/ReviewTransactionContainer'
 // import SignMessageModal from './SignMessageModal'
@@ -41,70 +31,24 @@ const RequestTypeSwitch = ({
   onCancel,
   onConfirm,
 }: RequestTypeSwitchProps) => {
-  const { t } = useTranslation()
-  const tokenPrices = useAppSelector(selectUsdPrices)
-
+  let ComponentToRender = null
   switch (request.type) {
-    // case 'signMessage':
-    //   return <SignMessageModal request={request} />
-    // case 'signTypedData':
-    //   return <SignTypedDataModal request={request} />
     case 'sendTransaction':
-      return (
-        <ReviewTransactionContainer
-          request={request}
-          onCancel={onCancel}
-          onConfirm={onConfirm}
-        />
-      )
+      ComponentToRender = ReviewTransactionContainer
+      break
     case 'SEND_BITCOIN':
-      const {
-        payload: { amountToPay, addressToPay, miningFee },
-      } = request
-      navigationContainerRef.navigate(rootTabsRouteNames.TransactionSummary, {
-        transaction: {
-          tokenValue: {
-            balance: convertSatoshiToBtcHuman(amountToPay),
-            symbolType: 'icon',
-            symbol: TokenSymbol.BTC,
-          },
-          usdValue: {
-            balance: convertTokenToUSD(
-              Number(convertSatoshiToBtcHuman(amountToPay)),
-              tokenPrices.BTC.price,
-              true,
-            ).toString(),
-            symbolType: 'text',
-            symbol: '$',
-          },
-          feeValue: convertSatoshiToBtcHuman(miningFee),
-          time: 'approx 1 min',
-          total: convertSatoshiToBtcHuman(amountToPay),
-        },
-        contact: {
-          address: addressToPay,
-        },
-        buttons: [
-          {
-            title: t('transaction_summary_title_confirm_button_title'),
-            onPress: () => {
-              request.confirm()
-              onConfirm(convertSatoshiToBtcHuman(amountToPay), TokenSymbol.BTC)
-            },
-            color: sharedColors.white,
-            textColor: sharedColors.black,
-          },
-          {
-            title: t('transaction_summary_title_cancel_button_title'),
-            onPress: onCancel,
-            backgroundVariety: AppButtonBackgroundVarietyEnum.OUTLINED,
-          },
-        ],
-      })
-      return null
+      ComponentToRender = ReviewBitcoinTransactionContainer
+      break
     default:
       return null
   }
+  return (
+    <ComponentToRender
+      request={request}
+      onCancel={onCancel}
+      onConfirm={onConfirm}
+    />
+  )
 }
 
 export const RequestHandler = ({ request, closeRequest }: Props) => {
