ddd43b2e72f7890dbe9873c8ad8323ad9b757b06
rodrigoncalves <rodrigosg2000@gmail.com>
2023-07-09 21:22:16 -0300

    adjust style and add some text

--- src/core/components/OfflineScreen.tsx
+++ src/core/components/OfflineScreen.tsx
@@ -5,15 +5,41 @@ import { sharedColors } from 'shared/constants'
 
 export const OfflineScreen = () => (
   <View style={styles.container}>
-    <Typography type="h1" color={sharedColors.dangerLight}>
-      Device is offline
-    </Typography>
+    <View style={styles.card}>
+      <Typography type="h3" color={sharedColors.white} style={styles.text}>
+        Ops! You are offline.
+      </Typography>
+      <Typography
+        type="body2"
+        color={sharedColors.labelLight}
+        style={styles.text}>
+        RIF Wallet needs an internet connection to work properly.
+      </Typography>
+      <Typography
+        type="body2"
+        color={sharedColors.labelLight}
+        style={styles.text}>
+        Please check your connection and try again.
+      </Typography>
+    </View>
   </View>
 )
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: sharedColors.black,
+    backgroundColor: sharedColors.secondary,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    backgroundColor: sharedColors.primary,
+    borderRadius: 25,
+    padding: 20,
+    margin: 20,
+  },
+  text: {
+    textAlign: 'center',
+    paddingVertical: 5,
   },
 })
