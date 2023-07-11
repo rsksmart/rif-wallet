112d996537afcb7213f891c58bf618ed42bfb71b
rodrigoncalves <rodrigosg2000@gmail.com>
2023-07-10 23:10:26 -0300

    change "check" icon to green

--- src/components/icons/SuccessIcon.tsx
+++ src/components/icons/SuccessIcon.tsx
@@ -9,7 +9,7 @@ export const SuccessIcon = (props: SvgProps) => (
       d="M23.629 97.138c0-43.63 35.37-79 79-79s79 35.37 79 79c0 43.631-35.37 79-79 79s-79-35.369-79-79Z"
     />
     <Path
-      stroke="#FFF"
+      stroke="#11B55C"
       strokeWidth={13}
       d="m74.95 96.309 19.005 19.005 36.352-36.352"
     />
