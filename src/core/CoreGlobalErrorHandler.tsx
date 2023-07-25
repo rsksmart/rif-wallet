db1ad688198e2dd55ce02544419e971da77c6f87
Alexander Evchenko <alexivchenko9@gmail.com>
2023-07-25 13:49:05 +0400

    feat: initialize magic instance

--- src/core/CoreGlobalErrorHandler.tsx
+++ src/core/CoreGlobalErrorHandler.tsx
@@ -1,12 +1,17 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Magic } from '@magic-sdk/react-native-bare'
+import { Config } from 'react-native-config'
 
 import { GlobalErrorHandler } from '../components/GlobalErrorHandler'
 import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary'
 import { CoreWithStore } from './CoreWithStore'
 
+export const magic = new Magic(Config.MAGIC_KEY)
+
 export const CoreGlobalErrorHandler = ({ CoreComponent = CoreWithStore }) => {
   return (
     <GlobalErrorHandler>
+      <magic.Relayer />
       <ErrorBoundary>
         <SafeAreaProvider>
           <CoreComponent />
