60944d84540dc6629a80f6695f224eb3a4f5167b
Alexander Evchenko <alexivchenko9@gmail.com>
2023-07-25 13:35:19 +0400

    feat: typing for the react-native-config

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+declare module 'react-native-config' {
+  interface Env {
+    APP_ENV: string
+    RIF_WALLET_SERVICE_URL: string
+    DEFAULT_CHAIN_TYPE: string
+    RIF_WALLET_KEY: string
+    MAGIC_KEY: string
+  }
+
+  export const Config: Env
+}
