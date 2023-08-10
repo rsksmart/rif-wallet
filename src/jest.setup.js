0055c3abcf0ab8133df3c9084dadb3243483017c
Rodrigo Gonçalves <rodrigo.santana@iovlabs.org>
2023-08-10 12:54:41 -0300

    i18n for tests

--- src/jest.setup.js
+++ src/jest.setup.js
@@ -1,4 +1,6 @@
 import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js'
 
+import 'lib/i18n'
+
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
 jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo)
