36d080943dbdb4fa11a41b7a6e1b901a26066b2d
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2023-08-14 08:31:06 -0300

    US-1868: Remove react-18next warning (#711)
    * remove react-18next warning
    
    * i18n for tests

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -377,15 +377,14 @@ const resources = {
   },
 }
 
-export const i18nInit = () =>
-  i18n
-    .use(initReactI18next) // passes i18n down to react-i18next
-    .init({
-      lng: 'en',
-      debug: true,
-      compatibilityJSON: 'v3',
-      resources,
-      interpolation: {
-        escapeValue: false, // react already safes from xss
-      },
-    })
+export default i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    lng: 'en',
+    debug: true,
+    compatibilityJSON: 'v3',
+    resources,
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  })
