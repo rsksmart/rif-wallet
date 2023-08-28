d1efa870bb6857303d2a329849422724d1a55234
Rodrigo Gonçalves <rodrigo.santana@iovlabs.org>
2023-08-28 00:46:49 -0300

    add usdrif logos

--- src/screens/home/TokenImage.tsx
+++ src/screens/home/TokenImage.tsx
@@ -65,6 +65,7 @@ export enum TokenSymbol {
   TRIF = 'tRIF',
   DOC = 'DOC',
   RDOC = 'RDOC',
+  USDRIF = 'USDRIF',
   RIFP = 'RIFP',
   BPRO = 'BPRO',
   RNS = 'RNS',
@@ -142,6 +143,8 @@ export const getTransparentIconSource = (
       return require('../../images/rif-white.png')
     case 'RDOC':
       return require('./../../images/rdoc-transparent.png')
+    case 'USDRIF':
+      return require('./../../images/usdrif-transparent.png')
     case 'BTCCB':
     case 'BTCT':
     case 'BTC':
@@ -168,6 +171,8 @@ export const getIconSource = (
       return require('@rsksmart/rsk-contract-metadata/images/doc.png')
     case 'RDOC':
       return require('./../../images/rdoc.png')
+    case 'USDRIF':
+      return require('./../../images/usdrif.png')
     case 'RIFP':
       return require('@rsksmart/rsk-contract-metadata/images/rifpro.png')
     case 'BPRO':
