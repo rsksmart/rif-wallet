f70587e01dbbaac5a13116103b0dc97cb7d7014d
Rodrigo Gonçalves <rodrigo.santana@iovlabs.org>
2023-09-14 17:54:46 -0300

    handle receive small amounts from dapp

--- src/ux/requestsModal/ReviewRelayTransaction/ReviewTransactionContainer.tsx
+++ src/ux/requestsModal/ReviewRelayTransaction/ReviewTransactionContainer.tsx
@@ -187,18 +187,19 @@ export const ReviewTransactionContainer = ({
     // get usd values
     const tokenUsd = convertToUSD(Number(value), tokenQuote)
     const feeUsd = convertToUSD(Number(feeValue), feeQuote)
+    const smallAmount = !Number(tokenUsd) && !!Number(value)
 
     return {
       transaction: {
         tokenValue: {
-          balance: value.toString(),
-          symbolType: 'icon',
           symbol: symbol || feeSymbol,
+          symbolType: 'icon',
+          balance: value.toString(),
         },
         usdValue: {
-          balance: tokenUsd,
+          symbol: smallAmount ? '<' : '$',
           symbolType: 'usd',
-          symbol: '$',
+          balance: smallAmount ? '0.01' : tokenUsd,
         },
         fee: {
           tokenValue: feeValue,
