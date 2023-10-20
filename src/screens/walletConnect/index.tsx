0ce1b1ee2cd6bbf5cdaebfea85c454bbc05dd459
Sleyter Sandoval <sleyter.sandoval@iovlabs.org>
2023-10-20 09:59:12 -0500

    other option for ios

--- src/screens/walletConnect/index.tsx
+++ src/screens/walletConnect/index.tsx
@@ -241,7 +241,7 @@ const styles = StyleSheet.create({
     flex: 4,
     alignSelf: 'center',
     width: '80%',
-    resizeMode: 'center',
+    resizeMode: 'contain',
   }),
   dappsList: castStyle.view({
     flex: 1,
