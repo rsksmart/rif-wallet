d5adde4226061f3ef7499a83bfb42324b656a49d
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2023-10-30 09:30:23 -0300

    Remove option to reset wallet if the user's authentication is incorrect (#786)

--- src/screens/createKeys/retryLogin/index.tsx
+++ src/screens/createKeys/retryLogin/index.tsx
@@ -1,19 +1,15 @@
-import { useCallback, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
+import { View } from 'react-native'
 
-import { sharedColors, sharedStyles } from 'shared/constants'
 import { AppButton } from 'components/index'
+import { sharedColors, sharedStyles } from 'shared/constants'
 import { unlockApp } from 'store/slices/settingsSlice'
-import { castStyle } from 'shared/utils'
-import { DeleteWalletModal } from 'components/modal/deleteWalletModal'
 import { useAppDispatch } from 'store/storeUtils'
 
 export const RetryLogin = () => {
   const dispatch = useAppDispatch()
   const { t } = useTranslation()
-  const [isDeleteConfirmationVisible, setIsDeleteConfirmationVisible] =
-    useState<boolean>(false)
 
   const retryLogin = useCallback(() => {
     dispatch(unlockApp({}))
@@ -27,23 +23,6 @@ export const RetryLogin = () => {
         color={sharedColors.white}
         textColor={sharedColors.black}
       />
-      <AppButton
-        onPress={() => setIsDeleteConfirmationVisible(true)}
-        title={t('initial_screen_button_erase_wallet')}
-        style={styles.btn}
-        color={sharedColors.danger}
-        textColor={sharedColors.white}
-      />
-      <DeleteWalletModal
-        isVisible={isDeleteConfirmationVisible}
-        setVisible={setIsDeleteConfirmationVisible}
-      />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  btn: castStyle.view({
-    marginTop: 12,
-  }),
-})
