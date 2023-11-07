da8fb1cd28a4a4fb34c1db0b35b68fe9d4c4cee7
Francis Rodriguez <francisngg@gmail.com>
2023-11-06 21:52:32 -0400

    US-1987 Added mining fees to the BitcoinMiningFeeInput

---
+++ src/ux/requestsModal/BitcoinMiningFeeContainer.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import { convertBtcToSatoshi } from '@rsksmart/rif-wallet-bitcoin'
+
+import { useAppSelector } from 'store/storeUtils'
+import { selectBitcoin } from 'store/slices/settingsSlice'
+import { Typography } from 'src/components'
+import { sharedStyles } from 'shared/styles'
+
+export const BitcoinMiningFeeContainer = () => {
+  const bitcoinCore = useAppSelector(selectBitcoin)
+  const [suggestedBitcoinFees, setSuggestedBitcoinFees] = useState<
+    { feeName: string; feeRate: string }[]
+  >([
+    { feeName: 'low', feeRate: '10000' },
+    { feeName: 'low', feeRate: '10000' },
+  ])
+
+  useEffect(() => {
+    // Fetch the bitcoin fees
+    bitcoinCore?.networksArr[0].bips[0].fetcher
+      .fetchBitcoinMiningFeeRates('cypher')
+      .then(cypherResponse => {
+        setSuggestedBitcoinFees([
+          {
+            feeName: 'low',
+            feeRate: cypherResponse.low_fee_per_kb.toString(),
+          },
+          {
+            feeName: 'medium',
+            feeRate: cypherResponse.medium_fee_per_kb.toString(),
+          },
+          {
+            feeName: 'high',
+            feeRate: cypherResponse.high_fee_per_kb.toString(),
+          },
+        ])
+      })
+      .catch(() => {
+        // Fetch from blockbook if cypher fails
+
+        bitcoinCore?.networksArr[0].bips[0].fetcher
+          .fetchBitcoinMiningFeeRates('blockbook')
+          .then(blockbookResponse => {
+            setSuggestedBitcoinFees([
+              {
+                feeName: 'low',
+                feeRate: convertBtcToSatoshi(
+                  blockbookResponse.result,
+                ).toString(),
+              },
+            ])
+          })
+      })
+  }, [bitcoinCore])
+
+  return (
+    <>
+      <Typography type="h4" style={sharedStyles.marginTop20}>
+        Fee rates:
+      </Typography>
+      {suggestedBitcoinFees.map(fee => (
+        <Typography
+          type="h4"
+          key={fee.feeName}
+          style={sharedStyles.marginTop10}>
+          {fee.feeName}: {fee.feeRate} satoshis
+        </Typography>
+      ))}
+    </>
+  )
+}
