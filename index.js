d652bebcdb51227cf47b3a7a05235b6bec6b89d7
Alexander Evchenko <alexivchenko9@gmail.com>
2025-05-23 15:08:38 +0400

    chore: make react-native successfully build on ios

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ import App from './src/App'
 import { name as appName } from './app.json'
 import './src/lib/i18n'
 
-console.log('TRACE ID', Config.TRACE_ID)
 
 AppRegistry.registerComponent(appName, () => App)
--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {}
+const defaultConfig = getDefaultConfig(__dirname)
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+// Define shims statically to avoid crashing before rn-nodeify runs
+const extraNodeModules = {
+  stream: path.resolve(__dirname, 'node_modules/stream-browserify'),
+  crypto: path.resolve(__dirname, 'node_modules/react-native-crypto'),
+  buffer: path.resolve(__dirname, 'node_modules/buffer'),
+  events: path.resolve(__dirname, 'node_modules/events'),
+  process: path.resolve(__dirname, 'node_modules/process/browser.js'),
+}
+
+const config = {
+  resolver: {
+    extraNodeModules,
+  },
+  watchFolders: [path.resolve(__dirname, 'node_modules')],
+}
+
+module.exports = mergeConfig(defaultConfig, config)
