6af55d5211fd57c81f8b0fa95dbb954aa52c2407
Alexander Evchenko <alexivchenko9@gmail.com>
2025-05-23 15:08:38 +0400

    chore: make react-native successfully build on ios

--- ios/rifWallet/AppDelegate.mm
+++ ios/rifWallet/AppDelegate.mm
@@ -2,12 +2,14 @@
 #import "RNBootSplash.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"rifWallet";
+  self.dependencyProvider = [RCTAppDependencyProvider new];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
